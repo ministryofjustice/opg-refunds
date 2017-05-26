f7cda1c99da380424f2cfe8e1c0e8264db23f855
Neil Smith <github@nsmith.me.uk>
2017-05-26 12:33:30 +0100

    Added Account Details form

--- src/App/src/Action/AccountDetailsAction.php
+++ src/App/src/Action/AccountDetailsAction.php
@@ -2,6 +2,8 @@
 
 namespace App\Action;
 
+use App\Form;
+
 use Interop\Http\ServerMiddleware\DelegateInterface;
 use Interop\Http\ServerMiddleware\MiddlewareInterface as ServerMiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -23,8 +25,28 @@ class AccountDetailsAction implements ServerMiddlewareInterface, Initializers\Te
     public function process(ServerRequestInterface $request, DelegateInterface $delegate)
     {
 
-        return new Response\HtmlResponse($this->getTemplateRenderer()->render('app::account-details-page', [
+        $form = new Form\AccountDetails();
 
+        if ($request->getMethod() == 'POST') {
+            $form->setData( $request->getParsedBody() );
+
+            if ($form->isValid()) {
+
+                $details = $request->getAttribute('session')->getArrayCopy();
+
+                // Merge the details into the rest of the data.
+                $details['account'] = $form->getData();
+
+                $reference = $this->applicationProcessService->process( $details );
+
+                return new Response\HtmlResponse($this->getTemplateRenderer()->render('app::done-page', [
+                    'reference' => $reference
+                ]));
+            }
+        }
+
+        return new Response\HtmlResponse($this->getTemplateRenderer()->render('app::account-details-page', [
+            'form' => $form
         ]));
     }
 
