d564781ffa9bc6640e077292a5f3084f6761f418
Neil Smith <github@nsmith.me.uk>
2017-08-18 15:17:38 +0100

    Set timezone

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('Europe/London');
+
 // Delegate static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server'
     && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))
