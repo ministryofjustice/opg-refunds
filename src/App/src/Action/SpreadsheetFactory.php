081b0d85e08a51c0cf9ed99a9e552d8e2b04d912
David Winchurch <david.winchurch@iterativeit.com>
2017-09-01 11:30:53 +0100

    REF-146: Basic download action

--- src/App/src/Action/SpreadsheetFactory.php
+++ src/App/src/Action/SpreadsheetFactory.php
@@ -2,12 +2,22 @@
 
 namespace App\Action;
 
+use App\Spreadsheet\PhpSpreadsheetGenerator;
+use App\Spreadsheet\SsclWorksheetGenerator;
 use Interop\Container\ContainerInterface;
 
 class SpreadsheetFactory
 {
     public function __invoke(ContainerInterface $container)
     {
-        return new SpreadsheetAction();
+        $config = $container->get('config');
+
+        $sourceFolder = $config['spreadsheet']['source_folder'];
+        $tempFolder = $config['spreadsheet']['temp_folder'];
+
+        $spreadsheetWorksheetGenerator = new SsclWorksheetGenerator();
+        $spreadsheetGenerator = new PhpSpreadsheetGenerator($sourceFolder, $tempFolder);
+
+        return new SpreadsheetAction($spreadsheetWorksheetGenerator, $spreadsheetGenerator);
     }
 }
