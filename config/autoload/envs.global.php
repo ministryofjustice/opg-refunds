972ba4690eafed14cc197c971094573caa1db0a3
David Winchurch <david.winchurch@iterativeit.com>
2017-09-05 06:49:29 +0100

    REF-204: Using new DB env vars

--- config/autoload/envs.global.php
+++ config/autoload/envs.global.php
@@ -42,11 +42,11 @@ return [
         'postgresql' => [
 
             'adapter' => 'pgsql',
-            'host' => getenv('API_DATABASE_HOSTNAME') ?: null,
-            'port' => getenv('API_DATABASE_PORT') ?: null,
-            'dbname' => getenv('API_DATABASE_NAME') ?: null,
-            'username' => getenv('API_DATABASE_USERNAME') ?: null,
-            'password' => getenv('API_DATABASE_PASSWORD') ?: null,
+            'host' => getenv('OPG_REFUNDS_DB_APPLICATIONS_HOSTNAME') ?: null,
+            'port' => getenv('OPG_REFUNDS_DB_APPLICATIONS_PORT') ?: null,
+            'dbname' => getenv('OPG_REFUNDS_DB_APPLICATIONS_NAME') ?: null,
+            'username' => getenv('OPG_REFUNDS_DB_APPLICATIONS_WRITE_USERNAME') ?: null,
+            'password' => getenv('OPG_REFUNDS_DB_APPLICATIONS_WRITE_PASSWORD') ?: null,
             'options' => [
                 PDO::ATTR_PERSISTENT => true
             ]
