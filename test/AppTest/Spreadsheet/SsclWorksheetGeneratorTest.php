6a5c131b9f4fd19d91cf61933315a57b04dbfb61
David Winchurch <david.winchurch@iterativeit.com>
2017-09-13 15:24:31 +0100

    REF-193: Updated spreadsheet test for new data

--- test/AppTest/Spreadsheet/SsclWorksheetGeneratorTest.php
+++ test/AppTest/Spreadsheet/SsclWorksheetGeneratorTest.php
@@ -5,11 +5,13 @@ namespace AppTest\Spreadsheet;
 use App\DataModel\Applications\Account;
 use App\DataModel\Cases\Payment;
 use App\DataModel\Cases\RefundCase as CaseDataModel;
+use App\Service\IdentFormatter;
 use App\Spreadsheet\ISpreadsheetWorksheetGenerator;
 use App\Spreadsheet\SpreadsheetRow;
 use App\Spreadsheet\SsclWorksheetGenerator;
 use AppTest\DataModel\Applications\ApplicationBuilder;
 use AppTest\DataModel\Cases\RefundCaseBuilder;
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 class SsclWorksheetGeneratorTest extends TestCase
@@ -100,29 +102,109 @@ class SsclWorksheetGeneratorTest extends TestCase
             }
 
             //Verify each cell contains the correct column and data
-            //reference
-            $this->assertEquals(4, $cells[0]->getColumn());
-            $this->assertEquals($case->getId(), $cells[0]->getData());
+            //OU
+            $this->assertEquals(0, $cells[0]->getColumn());
+            $this->assertEquals('MOJ', $cells[0]->getData());
 
-            //payeeName
-            $this->assertEquals(5, $cells[1]->getColumn());
-            $this->assertEquals($account->getName(), $cells[1]->getData());
+            //Payee Type
+            $this->assertEquals(3, $cells[1]->getColumn());
+            $this->assertEquals('Client', $cells[1]->getData());
 
-            //sortCode
-            $this->assertEquals(11, $cells[2]->getColumn());
-            $this->assertEquals($account->getSortCode(), $cells[2]->getData());
+            //Unique Payee Reference
+            $this->assertEquals(4, $cells[2]->getColumn());
+            $this->assertEquals(IdentFormatter::format($case->getId()), $cells[2]->getData());
 
-            //accountNumber
-            $this->assertEquals(12, $cells[3]->getColumn());
-            $this->assertEquals($account->getAccountNumber(), $cells[3]->getData());
+            //Payee Name
+            $this->assertEquals(5, $cells[3]->getColumn());
+            $this->assertEquals($account->getName(), $cells[3]->getData());
 
-            //amount
-            $this->assertEquals(26, $cells[4]->getColumn());
-            $this->assertEquals($payment->getAmount(), $cells[4]->getData());
+            //Payee Address (use commas to separate)
+            $this->assertEquals(6, $cells[4]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[4]->getData());
 
-            //amount
-            $this->assertEquals(28, $cells[5]->getColumn());
-            $this->assertEquals($payment->getAmount(), $cells[5]->getData());
+            //Payee Postcode
+            $this->assertEquals(7, $cells[5]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[5]->getData());
+
+            //Payment Method
+            $this->assertEquals(10, $cells[6]->getColumn());
+            $this->assertEquals('New Bank Details', $cells[6]->getData());
+
+            //Sort Code
+            $this->assertEquals(11, $cells[7]->getColumn());
+            $this->assertEquals($account->getSortCode(), $cells[7]->getData());
+
+            //Account Number
+            $this->assertEquals(12, $cells[8]->getColumn());
+            $this->assertEquals($account->getAccountNumber(), $cells[8]->getData());
+
+            //Name of Bank
+            $this->assertEquals(13, $cells[9]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[9]->getData());
+
+            //Account Name
+            $this->assertEquals(14, $cells[10]->getColumn());
+            $this->assertEquals($account->getName(), $cells[10]->getData());
+
+            //Roll Number
+            $this->assertEquals(15, $cells[11]->getColumn());
+            $this->assertEquals('N/A', $cells[11]->getData());
+
+            //Invoice Date
+            $this->assertEquals(16, $cells[12]->getColumn());
+            $this->assertEquals((new DateTime('today'))->format('d/m/Y'), $cells[12]->getData());
+
+            //Invoice Number
+            $this->assertEquals(17, $cells[13]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[13]->getData());
+
+            //Description
+            $this->assertEquals(18, $cells[14]->getColumn());
+            $this->assertEquals('Lasting Power of Attorney', $cells[14]->getData());
+
+            //Entity
+            $this->assertEquals(19, $cells[15]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[15]->getData());
+
+            //Cost Centre
+            $this->assertEquals(20, $cells[16]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[16]->getData());
+
+            //Account
+            $this->assertEquals(21, $cells[17]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[17]->getData());
+
+            //Objective
+            $this->assertEquals(22, $cells[18]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[18]->getData());
+
+            //Analysis
+            $this->assertEquals(23, $cells[19]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[19]->getData());
+
+            //VAT Rate
+            $this->assertEquals(24, $cells[20]->getColumn());
+            $this->assertEquals('UK OUT OF SCOPE', $cells[20]->getData());
+
+            //Net Amount
+            $this->assertEquals(26, $cells[21]->getColumn());
+            $this->assertEquals($payment->getAmount(), $cells[21]->getData());
+
+            //VAT Amount
+            $this->assertEquals(27, $cells[22]->getColumn());
+            $this->assertEquals(0, $cells[22]->getData());
+
+            //Total Amount
+            $this->assertEquals(28, $cells[23]->getColumn());
+            $this->assertEquals($payment->getAmount(), $cells[23]->getData());
+
+            //Completer ID
+            $this->assertEquals(29, $cells[24]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[24]->getData());
+
+            //Approver ID
+            $this->assertEquals(30, $cells[25]->getColumn());
+            $this->assertEquals('UNDEFINED', $cells[25]->getData());
         }
     }
 }
