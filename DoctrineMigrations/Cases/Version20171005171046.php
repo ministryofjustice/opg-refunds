bfc2ad2f03f807492d8d1346daac347a0eb8615a
David Winchurch <david.winchurch@iterativeit.com>
2017-10-05 18:17:08 +0100

    REF-332: Log -> not migration

---
+++ DoctrineMigrations/Cases/Version20171005171046.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DoctrineMigrations\Cases;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171005171046 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE log RENAME TO note');
+        $this->addSql('DROP SEQUENCE log_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE note_id_seq');
+        $this->addSql('SELECT setval(\'note_id_seq\', (SELECT MAX(id) FROM note))');
+        $this->addSql('ALTER TABLE note ALTER id SET DEFAULT nextval(\'note_id_seq\')');
+        $this->addSql('ALTER INDEX idx_8f3f68c57096a49f RENAME TO IDX_CFBDFA147096A49F');
+        $this->addSql('ALTER INDEX idx_8f3f68c5a76ed395 RENAME TO IDX_CFBDFA14A76ED395');
+        $this->addSql('ALTER INDEX idx_8f3f68c5bb18c0ba RENAME TO IDX_CFBDFA14BB18C0BA');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE note RENAME TO log');
+        $this->addSql('DROP SEQUENCE note_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE log_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('SELECT setval(\'log_id_seq\', (SELECT MAX(id) FROM log))');
+        $this->addSql('ALTER TABLE log ALTER id SET DEFAULT nextval(\'log_id_seq\')');
+        $this->addSql('ALTER INDEX idx_cfbdfa14bb18c0ba RENAME TO idx_8f3f68c5bb18c0ba');
+        $this->addSql('ALTER INDEX idx_cfbdfa147096a49f RENAME TO idx_8f3f68c57096a49f');
+        $this->addSql('ALTER INDEX idx_cfbdfa14a76ed395 RENAME TO idx_8f3f68c5a76ed395');
+    }
+}
