8b7173ca6e4baac676e23111d1555e949780438a
David Winchurch <david.winchurch@iterativeit.com>
2017-10-05 18:05:54 +0100

    REF-332: Datamodellog-note

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -29,7 +29,7 @@ final class Versions
   'guzzlehttp/promises' => 'v1.3.1@a59da6cf61d80060647ff4d3eb2c03a2bc694646',
   'guzzlehttp/psr7' => '1.4.2@f5b8a8512e2b58b0071a7280e39f14f72e05d87c',
   'http-interop/http-middleware' => '0.4.1@9a801fe60e70d5d608b61d56b2dcde29516c81b9',
-  'ministryofjustice/opg-refunds-caseworker-datamodels' => 'dev-master@4fcb6907e436ed955f45e1dff09f90cc2a93ae69',
+  'ministryofjustice/opg-refunds-caseworker-datamodels' => 'dev-REF-332-log-note@c3c2a2d6c5bf33c9b3ef0c7ce395837d5c20a988',
   'ministryofjustice/opg-refunds-logger' => 'v1.1.0@84fb465fa7eddd22e47c46de8111a46cf54cacfa',
   'mtdowling/jmespath.php' => '2.4.0@adcc9531682cf87dfda21e1fd5d0e7a41d292fac',
   'nikic/fast-route' => 'v1.2.0@b5f95749071c82a8e0f58586987627054400cdf6',
@@ -95,7 +95,7 @@ final class Versions
   'zendframework/zend-expressive-tooling' => '0.4.4@df464ec88c2040a2213dd6b84e842953bfc82c88',
   'zfcampus/zf-composer-autoloading' => '2.0.0@3643d9dc4d3f0b6011ff643672e1cf187e21a7f4',
   'zfcampus/zf-development-mode' => '3.1.0@ffef6ab8cf84ee1d1a77a2b51ba2240d2707c05d',
-  'ministryofjustice/opg-refunds-caseworker-api' => 'dev-REF-94-password_creation@278de907e0ecfe78c3f0cdc9a25e071f5308efc2',
+  'ministryofjustice/opg-refunds-caseworker-api' => 'dev-REF-332-log-note@ba425cad5a863ce7d2ba15b4682c55fd45aa9d59',
 );
 
     private function __construct()
