8abdf1df76df87cf15daf395e72dbbb62e9406a0
David Winchurch <david.winchurch@iterativeit.com>
2017-10-06 09:12:26 +0100

    REF-332: Further renaming of log -> note

--- src/App/src/Service/Claim/Claim.php
+++ src/App/src/Service/Claim/Claim.php
@@ -6,7 +6,7 @@ use Api\Service\Initializers\ApiClientInterface;
 use Api\Service\Initializers\ApiClientTrait;
 use Exception;
 use Opg\Refunds\Caseworker\DataModel\Cases\Claim as ClaimModel;
-use Opg\Refunds\Caseworker\DataModel\Cases\Log as LogModel;
+use Opg\Refunds\Caseworker\DataModel\Cases\Note as NoteModel;
 use Opg\Refunds\Caseworker\DataModel\Cases\Poa as PoaModel;
 use Opg\Refunds\Caseworker\DataModel\Cases\Verification as VerificationModel;
 
@@ -15,7 +15,7 @@ class Claim implements ApiClientInterface
     use ApiClientTrait;
 
     /**
-     * Retrieves the next available, unassigned Claim, assigns it to the currently logged in user and returns it
+     * Retrieves the next available, unassigned Claim, assigns it to the currently noteged in user and returns it
      *
      * @param int $userId user id to assign claim to
      * @return int the id of the next case to process. Will be zero if none was assigned
@@ -62,22 +62,22 @@ class Claim implements ApiClientInterface
 
     /**
      * @param int $claimId
-     * @param string $title the new log's title
-     * @param string $message the new log's message
-     * @return LogModel the newly created log
+     * @param string $title the new note's title
+     * @param string $message the new note's message
+     * @return NoteModel the newly created note
      */
-    public function addLog(int $claimId, string $title, string $message)
+    public function addNote(int $claimId, string $title, string $message)
     {
-        $logArray = $this->getApiClient()->httpPost("/v1/cases/claim/$claimId/log", [
+        $noteArray = $this->getApiClient()->httpPost("/v1/cases/claim/$claimId/note", [
             'title'   => $title,
             'message' => $message,
         ]);
 
-        if (empty($logArray)) {
+        if (empty($noteArray)) {
             return null;
         }
 
-        return new LogModel($logArray);
+        return new NoteModel($noteArray);
     }
 
     /**
