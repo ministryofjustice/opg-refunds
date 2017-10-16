81b0df7f2731e4745deb48d1df7c9ca6aaa9e33e
Neil Smith <github@nsmith.me.uk>
2017-10-16 10:28:29 +0100

    Removed full payload encryption

--- src/App/src/Service/Refund/Data/DataHandlerFactory.php
+++ src/App/src/Service/Refund/Data/DataHandlerFactory.php
@@ -31,23 +31,8 @@ class DataHandlerFactory
         // Set PDO to throw exceptions on error
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-
-        //-------------------------------------
-        // Encryption - Everything
-
-        if (!isset($config['security']['rsa']['keys']['public']['full'])) {
-            throw new \UnexpectedValueException('Bank RSA public key is not configured');
-        }
-
-        $keyPath = $config['security']['rsa']['keys']['public']['full'];
-
-        $cipher = new HybridCipher(
-            BlockCipher::factory('openssl', ['algo' => 'aes']),
-            Rsa::factory(['public_key'=> $keyPath])
-        );
-
         //---
 
-        return new DataHandlerLocal($db, $cipher);
+        return new DataHandlerLocal($db);
     }
 }
