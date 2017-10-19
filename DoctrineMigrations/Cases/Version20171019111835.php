9b392317eabc1358121b10c1e9be2318e16bcd84
David Winchurch <david.winchurch@iterativeit.com>
2017-10-19 12:19:18 +0100

    REF-407: Original payment amount can be null

---
+++ DoctrineMigrations/Cases/Version20171019111835.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DoctrineMigrations\Cases;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171019111835 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE poa ALTER original_payment_amount DROP NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE poa ALTER original_payment_amount SET NOT NULL');
+    }
+}
