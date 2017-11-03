bc742b939dbf923dfac9e06f757b4d5cef792b85
David Winchurch <david.winchurch@iterativeit.com>
2017-11-03 12:17:22 +0000

    REF-414: Migration script incorrectly escaped encrypted bank details

--- DoctrineMigrations/Cases/Version20171103093007.php
+++ DoctrineMigrations/Cases/Version20171103093007.php
@@ -18,7 +18,7 @@ class Version20171103093007 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE claim ALTER json_data TYPE JSON USING encode(json_data, \'escape\')::json');
+        $this->addSql('ALTER TABLE claim ALTER json_data TYPE JSON USING replace(encode(json_data, \'escape\'), \'\\\\\', \'\\\')::json');
         $this->addSql('ALTER TABLE claim ALTER json_data DROP DEFAULT');
     }
 
