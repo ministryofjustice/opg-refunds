70b3f03f1ef732c52b43f86df2fed73b847eb381
David Winchurch <david.winchurch@iterativeit.com>
2017-11-03 15:36:10 +0000

    REF-414: Fixing rebase issues

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -100,7 +100,7 @@ final class Versions
   'zendframework/zend-expressive-tooling' => '0.4.4@df464ec88c2040a2213dd6b84e842953bfc82c88',
   'zfcampus/zf-composer-autoloading' => '2.0.0@3643d9dc4d3f0b6011ff643672e1cf187e21a7f4',
   'zfcampus/zf-development-mode' => '3.1.0@ffef6ab8cf84ee1d1a77a2b51ba2240d2707c05d',
-  'ministryofjustice/opg-refunds-caseworker-api' => 'dev-REF-414-sop1@1d54ca61721c7cad9ba54b69bcb78b256684b2b0',
+  'ministryofjustice/opg-refunds-caseworker-api' => 'dev-REF-414-sop1@dc201ab83746f873ddb2979d1ba866070f43f907',
 );
 
     private function __construct()
