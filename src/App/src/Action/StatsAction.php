dcf3ae00e0e5083fa57b175f762c332f210ae953
David Winchurch <david.winchurch@iterativeit.com>
2017-11-17 16:51:31 +0000

    REF-574: Dummy stats action

--- src/App/src/Action/StatsAction.php
+++ src/App/src/Action/StatsAction.php
@@ -2,11 +2,14 @@
 
 namespace App\Action;
 
+use DateTime;
 use Doctrine\ORM\EntityManager;
 use Interop\Http\ServerMiddleware\DelegateInterface;
+use Opg\Refunds\Caseworker\DataModel\AbstractDataModel;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
+use Zend\Diactoros\Response\JsonResponse;
 
 /**
  * Class StatsAction
@@ -31,6 +34,10 @@ class StatsAction extends AbstractRestfulAction
      */
     public function indexAction(ServerRequestInterface $request, DelegateInterface $delegate)
     {
+        $generated = new DateTime();
 
+        return new JsonResponse([
+            'generated' => date('d/m/Y H:i:s', $generated->getTimestamp())
+        ]);
     }
 }
