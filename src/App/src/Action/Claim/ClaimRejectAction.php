9f6cde5a6b9fa97ccd407260280ce6d6d5228bcc
David Winchurch <david.winchurch@iterativeit.com>
2017-11-22 16:05:19 +0000

    REF-584: No longer email or text users on rejection/acceptance

--- src/App/src/Action/Claim/ClaimRejectAction.php
+++ src/App/src/Action/Claim/ClaimRejectAction.php
@@ -88,61 +88,6 @@ class ClaimRejectAction extends AbstractClaimAction
                 throw new RuntimeException('Failed to set rejection reason on claim with id: ' . $this->modelId);
             }
 
-            $sendRejectionMessage = true;
-            $smsTemplate = false;
-
-            $emailPersonalisation = [
-                'no-poas-found'         => 'no',
-                'no-fees-paid'          => 'no',
-                'poas-already-refunded' => 'no',
-                'details-not-verified'  => 'no',
-            ];
-
-            switch ($claim->getRejectionReason()) {
-                case ClaimModel::REJECTION_REASON_NO_ELIGIBLE_POAS_FOUND:
-                    $emailPersonalisation['no-poas-found'] = 'yes';
-                    $smsTemplate = 'f90cdca8-cd8b-4e22-ac66-d328b219f53e';
-                    break;
-                case ClaimModel::REJECTION_REASON_PREVIOUSLY_REFUNDED:
-                    $emailPersonalisation['poas-already-refunded'] = 'yes';
-                    $smsTemplate = '5ccfdd66-0040-423a-8426-1458f912d41a';
-                    break;
-                case ClaimModel::REJECTION_REASON_NO_FEES_PAID:
-                    $emailPersonalisation['no-fees-paid'] = 'yes';
-                    $smsTemplate = '80b81c91-667e-47d8-bd8e-b87fdfa1b3de';
-                    break;
-                case ClaimModel::REJECTION_REASON_CLAIM_NOT_VERIFIED:
-                    $emailPersonalisation['details-not-verified'] = 'yes';
-                    $smsTemplate = '2bb54224-0cab-44b9-9623-fd12f6ee6e77';
-                    break;
-                case ClaimModel::REJECTION_REASON_OTHER:
-                default:
-                    $sendRejectionMessage = false;
-            }
-
-            if ($sendRejectionMessage) {
-                $contact = $claim->getApplication()->getContact();
-                $contactName = $claim->getApplication()->getApplicant() === 'attorney' ?
-                    DetailsFormatterPlatesExtension::getFormattedName(
-                        $claim->getApplication()->getAttorney()->getCurrent()->getName()
-                    ) : $claim->getDonorName();
-
-                if ($contact->hasEmail()) {
-                    $this->notifyClient->sendEmail($contact->getEmail(), '018ab571-a2a5-41e6-a1d4-ae369e2d3cd1', array_merge($emailPersonalisation, [
-                        'person-completing' => $contactName,
-                        'donor-name' => $claim->getDonorName(),
-                        'claim-code' => $claim->getReferenceNumber()
-                    ]));
-                }
-
-                if ($contact->hasPhone() && substr($contact->getPhone(), 0, 2) === '07' && $smsTemplate) {
-                    $this->notifyClient->sendSms($contact->getPhone(), $smsTemplate, [
-                        'donor-name' => $claim->getDonorName(),
-                        'claim-code' => $claim->getReferenceNumber()
-                    ]);
-                }
-            }
-
             return $this->redirectToRoute('home');
         }
 
