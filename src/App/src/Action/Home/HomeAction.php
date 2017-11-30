b66c74cb5a2d630dc731f1c2a8f0b30b4e125514
David Winchurch <david.winchurch@iterativeit.com>
2017-11-30 14:05:15 +0000

    Only display links you are assigned the role for. Redirect to correct page based on role

--- src/App/src/Action/Home/HomeAction.php
+++ src/App/src/Action/Home/HomeAction.php
@@ -9,6 +9,7 @@ use Interop\Http\ServerMiddleware\DelegateInterface;
 use Opg\Refunds\Caseworker\DataModel\Cases\User;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\RedirectResponse;
 
 /**
  * Class HomeAction
@@ -33,25 +34,36 @@ class HomeAction extends AbstractAction
     /**
      * @param ServerRequestInterface $request
      * @param DelegateInterface $delegate
-     * @return HtmlResponse
+     * @return HtmlResponse|RedirectResponse
      */
     public function process(ServerRequestInterface $request, DelegateInterface $delegate)
     {
         /** @var User $identity */
         $identity = $request->getAttribute('identity');
 
-        //  Even though the user details are in the session get them again with a GET call to the API
-        $user = $this->userService->getUser($identity->getId());
+        if (in_array(User::ROLE_CASEWORKER, $identity->getRoles())) {
+            //  Even though the user details are in the session get them again with a GET call to the API
+            $user = $this->userService->getUser($identity->getId());
 
-        $session = $request->getAttribute('session');
-        $form = new ProcessNewClaim([
-            'csrf' => $session['meta']['csrf'],
-        ]);
+            $session = $request->getAttribute('session');
+            $form = new ProcessNewClaim([
+                'csrf' => $session['meta']['csrf'],
+            ]);
 
-        return new HtmlResponse($this->getTemplateRenderer()->render('app::home-page', [
-            'form'     => $form,
-            'user'     => $user,
-            'messages' => $this->getFlashMessages($request)
-        ]));
+            return new HtmlResponse($this->getTemplateRenderer()->render('app::home-page', [
+                'form' => $form,
+                'user' => $user,
+                'messages' => $this->getFlashMessages($request)
+            ]));
+        } elseif (in_array(User::ROLE_REPORTING, $identity->getRoles())) {
+            return $this->redirectToRoute('reporting');
+        } elseif (in_array(User::ROLE_REFUND, $identity->getRoles())) {
+            return $this->redirectToRoute('refund');
+        } elseif (in_array(User::ROLE_ADMIN, $identity->getRoles())) {
+            return $this->redirectToRoute('user');
+        }
+
+        //No roles!
+        return $this->redirectToRoute('sign.out');
     }
 }
