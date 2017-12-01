5703460bbf0c82bdd35ca44a0c05e7b3b3930317
Neil Smith <github@nsmith.me.uk>
2017-12-01 16:33:37 +0000

    Added cookie check action

--- src/App/src/Action/WhenFeesPaidAction.php
+++ src/App/src/Action/WhenFeesPaidAction.php
@@ -23,7 +23,7 @@ class WhenFeesPaidAction extends AbstractAction
             if ($form->isValid()) {
                 if ($form->getData()['fees-in-range'] === 'yes') {
                     return new Response\RedirectResponse(
-                        $this->getUrlHelper()->generate('apply.who')
+                        $this->getUrlHelper()->generate('cookies.check')
                     );
                 }
 
