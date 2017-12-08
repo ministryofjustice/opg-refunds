0a0560282b3e472610edb060870ed8d5bad29fa3
David Winchurch <david.winchurch@iterativeit.com>
2017-12-08 08:21:55 +0000

    REF-623: Correctly identify claims to contact by phone

--- src/App/src/Service/Notify.php
+++ src/App/src/Service/Notify.php
@@ -52,11 +52,16 @@ class Notify implements Initializer\LogSupportInterface
                 SELECT id, finished_datetime FROM claim
                 WHERE outcome_text_sent IS NOT TRUE
                 AND ((status IN (:statusDuplicate, :statusRejected) AND finished_datetime < :today) OR (status = :acceptedStatus AND payment_id IS NOT NULL))
-                AND json_data->\'contact\'->\'phone\' IS NOT NULL UNION
+                AND json_data->\'contact\'->\'phone\' IS NOT NULL AND (json_data->>\'contact\')::json->>\'phone\' LIKE \'07%\' UNION
                 SELECT id, finished_datetime FROM claim
                 WHERE outcome_letter_sent IS NOT TRUE
                 AND ((status IN (:statusDuplicate, :statusRejected) AND finished_datetime < :today) OR (status = :acceptedStatus AND payment_id IS NOT NULL))
-                AND json_data->\'contact\'->\'address\' IS NOT NULL
+                AND json_data->\'contact\'->\'address\' IS NOT NULL UNION
+                SELECT id, finished_datetime FROM claim
+                WHERE outcome_phone_called IS NOT TRUE
+                AND ((status IN (:statusDuplicate, :statusRejected) AND finished_datetime < :today) OR (status = :acceptedStatus AND payment_id IS NOT NULL))
+                AND json_data->\'contact\'->\'email\' IS NULL AND json_data->\'contact\'->\'address\' IS NULL
+                AND json_data->\'contact\'->\'phone\' IS NOT NULL AND (json_data->>\'contact\')::json->>\'phone\' NOT LIKE \'07%\'
                 ORDER BY finished_datetime';
 
         $statement = $this->entityManager->getConnection()->executeQuery(
@@ -133,7 +138,7 @@ class Notify implements Initializer\LogSupportInterface
 
         //Only send back letter and phone call details if there are no more emails or texts to send
         //so that sending the letters and making the calls are the last tasks to complete
-        if ($notified['total'] - $notified['processed'] - count($letters) === 0) {
+        if ($notified['total'] - $notified['processed'] - count($letters) - count($phoneCalls) === 0) {
             $notified['letters'] = $letters;
             $notified['phoneCalls'] = $phoneCalls;
         } else {
