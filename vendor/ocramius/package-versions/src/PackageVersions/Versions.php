48f454416d3345550cda6429ec79af787bddb4f1
David Winchurch <david.winchurch@iterativeit.com>
2018-01-15 09:34:40 +0000

    Merge fix

--- vendor/ocramius/package-versions/src/PackageVersions/Versions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Versions.php
@@ -106,7 +106,7 @@ final class Versions
   'zendframework/zend-expressive-tooling' => '0.4.4@df464ec88c2040a2213dd6b84e842953bfc82c88',
   'zfcampus/zf-composer-autoloading' => '2.0.0@3643d9dc4d3f0b6011ff643672e1cf187e21a7f4',
   'zfcampus/zf-development-mode' => '3.1.0@ffef6ab8cf84ee1d1a77a2b51ba2240d2707c05d',
-  'ministryofjustice/opg-refunds-caseworker-api' => 'dev-ingestion-daemon@508ccd730bba943e4569aba78dc076066ba185d9',
+  'ministryofjustice/opg-refunds-caseworker-api' => 'dev-ingestion-daemon@5ddc2bc641f49b87b6e25331c02bd00277d8e040',
 );
 
     private function __construct()
