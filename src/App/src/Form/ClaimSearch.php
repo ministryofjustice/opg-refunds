46e5a721b6d02c4c591a0b96cfab2b38c4ab2b1b
David Winchurch <david.winchurch@iterativeit.com>
2018-03-08 15:01:43 +0000

    REF-804: Add withdrawn back to completed

--- src/App/src/Form/ClaimSearch.php
+++ src/App/src/Form/ClaimSearch.php
@@ -51,7 +51,7 @@ class ClaimSearch extends AbstractForm
 
         $field->setValueOptions([
             '' => 'All',
-            join(',', [ClaimModel::STATUS_DUPLICATE, ClaimModel::STATUS_REJECTED, ClaimModel::STATUS_ACCEPTED]) => 'Completed',
+            join(',', [ClaimModel::STATUS_DUPLICATE, ClaimModel::STATUS_REJECTED, ClaimModel::STATUS_ACCEPTED, ClaimModel::STATUS_WITHDRAWN]) => 'Completed',
             ClaimModel::STATUS_PENDING => StatusFormatter::getStatusText(ClaimModel::STATUS_PENDING),
             ClaimModel::STATUS_IN_PROGRESS => StatusFormatter::getStatusText(ClaimModel::STATUS_IN_PROGRESS),
             ClaimModel::STATUS_DUPLICATE => StatusFormatter::getStatusText(ClaimModel::STATUS_DUPLICATE),
