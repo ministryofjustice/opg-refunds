2ed96ae4f7f9875612f4d27fbdd75681f26cb2c4
Neil Smith <github@nsmith.me.uk>
2018-03-21 11:01:46 +0000

    Revert single-use watch on form submission

--- src/js/app/single-use.js
+++ src/js/app/single-use.js
@@ -5,25 +5,14 @@
     var GOVUK = global.GOVUK || {}
 
     var SingleUse = function (selector) {
-        $(selector).on('click', this.handleClick)
         $(selector).closest('form').on('submit', this.handleSubmit)
-    }
-
-    SingleUse.prototype.handleClick = function(e) {
-        var $button = $(e.target);
-
-        // Disable submit button
-        $button.attr('disabled', 'disabled').attr('aria-disabled', 'true');
-
-        // Ensure the form is still submitted
-        $button.closest('form').submit()
     }
 
     SingleUse.prototype.handleSubmit = function(e) {
         var $form = $(e.target);
 
         // Disable submit button
-        $form.find('.js-single-use').attr('aria-disabled', 'true');
+        $form.find('.js-single-use').attr('disabled', 'disabled').attr('aria-disabled', 'true');
     }
 
     GOVUK.SingleUse = SingleUse
