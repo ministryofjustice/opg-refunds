48b6fedc8faf01811a40ae2646046aa8e4edfab0
David Winchurch <david.winchurch@iterativeit.com>
2018-06-29 10:31:52 +0100

    LPA-2758: Migration to Zend Expressive v2.2 for all services

--- public-front/config/pipeline.php
+++ public-front/config/pipeline.php
@@ -2,14 +2,18 @@
 
 use Zend\Expressive\Helper\ServerUrlMiddleware;
 use Zend\Expressive\Helper\UrlHelperMiddleware;
-use Zend\Expressive\Middleware\ImplicitHeadMiddleware;
-use Zend\Expressive\Middleware\ImplicitOptionsMiddleware;
+use Zend\Expressive\Router\Middleware\DispatchMiddleware;
+use Zend\Expressive\Router\Middleware\ImplicitHeadMiddleware;
+use Zend\Expressive\Router\Middleware\ImplicitOptionsMiddleware;
+use Zend\Expressive\Router\Middleware\RouteMiddleware;
 use Zend\Expressive\Middleware\NotFoundHandler;
 use Zend\Stratigility\Middleware\ErrorHandler;
 
 /**
  * Setup middleware pipeline:
  */
+
+/** @var \Zend\Expressive\Application $app */
 
 // The error handler should be the first (most outer) middleware to catch
 // all Exceptions.
@@ -34,7 +38,7 @@ $app->pipe(ServerUrlMiddleware::class);
 // - $app->pipe('/files', $filesMiddleware);
 
 // Register the routing middleware in the middleware pipeline
-$app->pipeRoutingMiddleware();
+$app->pipe(RouteMiddleware::class);
 $app->pipe(ImplicitHeadMiddleware::class);
 $app->pipe(ImplicitOptionsMiddleware::class);
 $app->pipe(UrlHelperMiddleware::class);
@@ -61,7 +65,7 @@ foreach (['/session-finished', '/application', '/assisted-digital', '/beta'] as 
 // - etc.
 
 // Register the dispatch middleware in the middleware pipeline
-$app->pipeDispatchMiddleware();
+$app->pipe(DispatchMiddleware::class);
 
 // At this point, if no Response is return by any middleware, the
 // NotFoundHandler kicks in; alternately, you can provide other fallback
