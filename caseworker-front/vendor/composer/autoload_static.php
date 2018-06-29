c618721c9adc3fd9f79121eca0f204342fa25b67
David Winchurch <david.winchurch@iterative-it.com>
2018-06-29 12:18:14 +0000

    LPA-2830: Ready to run zend expressive 3 migration script

--- caseworker-front/vendor/composer/autoload_static.php
+++ caseworker-front/vendor/composer/autoload_static.php
@@ -20,14 +20,14 @@ class ComposerStaticInitc217723c52a7a32304fe0576dd81a285
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
         'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
+        '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         'c0c684e103c597a31a3e514398f79ec1' => __DIR__ . '/..' . '/zendframework/zend-stratigility/src/functions/double-pass-middleware.php',
         '73f02b5e05532acfb968fa91193a3d17' => __DIR__ . '/..' . '/zendframework/zend-stratigility/src/functions/middleware.php',
         '7ba36b1c7d480f53d620df374623203a' => __DIR__ . '/..' . '/zendframework/zend-stratigility/src/functions/path.php',
-        '253c157292f75eb38082b5acb06f3f01' => __DIR__ . '/..' . '/nikic/fast-route/src/functions.php',
         'b067bc7112e384b61c701452d53a14a8' => __DIR__ . '/..' . '/mtdowling/jmespath.php/src/JmesPath.php',
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '8a9dc1de0ca7e01f3e08231539562f61' => __DIR__ . '/..' . '/aws/aws-sdk-php/src/functions.php',
         '6124b4c8570aa390c21fafd04a26c69f' => __DIR__ . '/..' . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
+        '8a9dc1de0ca7e01f3e08231539562f61' => __DIR__ . '/..' . '/aws/aws-sdk-php/src/functions.php',
         '34901568a1e26d13dd475cb2c85e0284' => __DIR__ . '/..' . '/zendframework/zend-form/autoload/formElementManagerPolyfill.php',
     );
 
@@ -45,7 +45,6 @@ class ComposerStaticInitc217723c52a7a32304fe0576dd81a285
             'Zend\\ServiceManager\\' => 20,
             'Zend\\Permissions\\Rbac\\' => 22,
             'Zend\\Math\\' => 10,
-            'Zend\\Log\\' => 9,
             'Zend\\InputFilter\\' => 17,
             'Zend\\Hydrator\\' => 14,
             'Zend\\Form\\' => 10,
@@ -54,6 +53,7 @@ class ComposerStaticInitc217723c52a7a32304fe0576dd81a285
             'Zend\\Expressive\\Template\\' => 25,
             'Zend\\Expressive\\Router\\' => 23,
             'Zend\\Expressive\\Plates\\' => 23,
+            'Zend\\Expressive\\Migration\\' => 26,
             'Zend\\Expressive\\Helper\\' => 23,
             'Zend\\Expressive\\' => 16,
             'Zend\\EventManager\\' => 18,
@@ -87,7 +87,6 @@ class ComposerStaticInitc217723c52a7a32304fe0576dd81a285
         ),
         'O' => 
         array (
-            'Opg\\Refunds\\Log\\' => 16,
             'Opg\\Refunds\\Caseworker\\DataModel\\' => 33,
         ),
         'L' => 
@@ -176,10 +175,6 @@ class ComposerStaticInitc217723c52a7a32304fe0576dd81a285
         array (
             0 => __DIR__ . '/..' . '/zendframework/zend-math/src',
         ),
-        'Zend\\Log\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/zendframework/zend-log/src',
-        ),
         'Zend\\InputFilter\\' => 
         array (
             0 => __DIR__ . '/..' . '/zendframework/zend-inputfilter/src',
@@ -212,6 +207,10 @@ class ComposerStaticInitc217723c52a7a32304fe0576dd81a285
         'Zend\\Expressive\\Plates\\' => 
         array (
             0 => __DIR__ . '/..' . '/zendframework/zend-expressive-platesrenderer/src',
+        ),
+        'Zend\\Expressive\\Migration\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/zendframework/zend-expressive-migration/src',
         ),
         'Zend\\Expressive\\Helper\\' => 
         array (
@@ -296,10 +295,6 @@ class ComposerStaticInitc217723c52a7a32304fe0576dd81a285
         'Psr\\Container\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/container/src',
-        ),
-        'Opg\\Refunds\\Log\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/ministryofjustice/opg-refunds-logger/src/Opg/Refunds/Log',
         ),
         'Opg\\Refunds\\Caseworker\\DataModel\\' => 
         array (
