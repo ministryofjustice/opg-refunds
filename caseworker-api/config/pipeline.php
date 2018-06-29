13c66761a1f15513f1a00b74c735bd55098a1d20
David Winchurch <david.winchurch@iterativeit.com>
2018-06-29 10:31:52 +0100

    LPA-2758: Migration to Zend Expressive v2.2 for all services

--- caseworker-api/config/pipeline.php
+++ caseworker-api/config/pipeline.php
@@ -4,14 +4,18 @@ use App\Middleware;
 use Auth\Middleware as AuthMiddleware;
 use Zend\Expressive\Helper\BodyParams\BodyParamsMiddleware;
 use Zend\Expressive\Helper\ServerUrlMiddleware;
-use Zend\Expressive\Middleware\ImplicitHeadMiddleware;
-use Zend\Expressive\Middleware\ImplicitOptionsMiddleware;
+use Zend\Expressive\Router\Middleware\DispatchMiddleware;
+use Zend\Expressive\Router\Middleware\ImplicitHeadMiddleware;
+use Zend\Expressive\Router\Middleware\ImplicitOptionsMiddleware;
+use Zend\Expressive\Router\Middleware\RouteMiddleware;
 use Zend\Expressive\Middleware\NotFoundHandler;
 use Zend\Stratigility\Middleware\ErrorHandler;
 
 /**
  * Setup middleware pipeline:
  */
+
+/** @var \Zend\Expressive\Application $app */
 
 // The error handler should be the first (most outer) middleware to catch
 // all Exceptions.
@@ -36,7 +40,7 @@ $app->pipe(ServerUrlMiddleware::class);
 // - $app->pipe('/files', $filesMiddleware);
 
 // Register the routing middleware in the middleware pipeline
-$app->pipeRoutingMiddleware();
+$app->pipe(RouteMiddleware::class);
 $app->pipe(ImplicitHeadMiddleware::class);
 $app->pipe(ImplicitOptionsMiddleware::class);
 $app->pipe(BodyParamsMiddleware::class);
@@ -48,7 +52,7 @@ $app->pipe(Middleware\ProblemDetailsMiddleware::class);
 $app->pipe(AuthMiddleware\AuthorizationMiddleware::class);
 
 // Register the dispatch middleware in the middleware pipeline
-$app->pipeDispatchMiddleware();
+$app->pipe(DispatchMiddleware::class);
 
 // At this point, if no Response is return by any middleware, the
 // NotFoundHandler kicks in; alternately, you can provide other fallback
