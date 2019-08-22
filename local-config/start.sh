845a8e96a4f945310f4356ae2eb1e6e179c37cb4
Neil Smith <github@nsmith.me.uk>
2019-08-22 16:43:16 +0100

    Added initial containers

---
+++ local-config/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo Starting local config
+
+DYNAMODN_ENDPOINT=http://${AWS_ENDPOINT_DYNAMODB}
+/usr/local/bin/waitforit -address=tcp://${AWS_ENDPOINT_DYNAMODB} -timeout 60 -retry 6000 -debug
+
+# ----------------------------------------------------------
+# Add any setup here that is performed with Terraform in AWS.
+
+aws dynamodb create-table \
+--attribute-definitions AttributeName=id,AttributeType=S \
+--table-name Sessions \
+--key-schema AttributeName=id,KeyType=HASH \
+--provisioned-throughput ReadCapacityUnits=100,WriteCapacityUnits=100 \
+--region eu-west-1 \
+--endpoint $DYNAMODN_ENDPOINT
