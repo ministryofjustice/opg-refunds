e3dc6df9af2112a8507fdce2dfae175e484aafbb
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-08-29 02:31:11 +0100

    set up credentials for accounts

---
+++ terraform/account/locals.tf
@@ -0,0 +1,37 @@
+# variables for terraform.tfvars.json
+variable "account_mapping" {
+  type = "map"
+}
+
+variable "accounts" {
+  type = map(
+    object({
+      account_id    = string
+      is_production = string
+    })
+  )
+}
+
+locals {
+  opg_project = "lpa refunds"
+
+  account_name = lookup(var.account_mapping, terraform.workspace, "development")
+  account      = var.accounts[local.account_name]
+
+
+  mandatory_moj_tags = {
+    business-unit = "OPG"
+    application   = "Claim a Lasting Power of Attorney Refund"
+    owner         = "Amy Wilson: amy.wilson@digital.justice.gov.uk"
+    is-production = local.account.is_production
+  }
+
+  optional_tags = {
+    environment-name       = local.account_name
+    infrastructure-support = "OPG LPA Product Team: opgteam+online-lpa@digital.justice.gov.uk"
+    runbook                = "https://github.com/ministryofjustice/opg-webops-runbooks/tree/master/LPA"
+    source-code            = "https://github.com/ministryofjustice/opg-lpa"
+  }
+
+  default_tags = merge(local.mandatory_moj_tags, local.optional_tags)
+}
