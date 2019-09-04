ae89df662bd1b43da4bd6c0d362cb83f62fd3248
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-09-04 18:47:34 +0100

    tf fmt config file

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -6,10 +6,10 @@ resource "local_file" "environment_pipeline_tasks_config" {
 locals {
   public_front_url = "https://${aws_route53_record.public_front.name}"
   environment_pipeline_tasks_config = {
-    account_id            = local.account.account_id
-    cluster_name          = aws_ecs_cluster.lpa_refunds.name
+    account_id           = local.account.account_id
+    cluster_name         = aws_ecs_cluster.lpa_refunds.name
     public_front_url     = local.public_front_url
     caseworker_front_url = "nothing here yet"
-    tag                   = var.container_version
+    tag                  = var.container_version
   }
 }
