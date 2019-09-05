e2c7072c5d1829c12b3116a13af5b4e669624500
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-09-05 11:27:55 +0100

    fix public front fqdn in config file

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -7,7 +7,7 @@ locals {
   environment_pipeline_tasks_config = {
     account_id            = local.account.account_id
     cluster_name          = aws_ecs_cluster.lpa_refunds.name
-    public_front_fqdn     = aws_route53_record.public_front.name
+    public_front_fqdn     = aws_route53_record.public_front.fqdn
     caseworker_front_fqdn = "nothing here yet"
     tag                   = var.container_version
   }
