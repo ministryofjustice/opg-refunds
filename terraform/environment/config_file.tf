2c84a8d12143d485e1a91bee32dea69828c18713
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-09-05 11:12:07 +0100

    fix fqdns in config file

--- terraform/environment/config_file.tf
+++ terraform/environment/config_file.tf
@@ -4,11 +4,10 @@ resource "local_file" "environment_pipeline_tasks_config" {
 }
 
 locals {
-  public_front_url = "https://${aws_route53_record.public_front.name}"
   environment_pipeline_tasks_config = {
     account_id            = local.account.account_id
     cluster_name          = aws_ecs_cluster.lpa_refunds.name
-    public_front_fqdn     = local.public_front_url
+    public_front_fqdn     = aws_route53_record.public_front.name
     caseworker_front_fqdn = "nothing here yet"
     tag                   = var.container_version
   }
