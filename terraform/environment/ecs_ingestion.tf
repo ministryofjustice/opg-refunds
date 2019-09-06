45698c4783a065bdcb3932ff46ed3993f76ac506
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-09-06 14:10:33 +0100

    allow egress for the container to be ableto talk to ecr

--- terraform/environment/ecs_ingestion.tf
+++ terraform/environment/ecs_ingestion.tf
@@ -31,14 +31,14 @@ resource "aws_security_group" "ingestion_ecs_service" {
 
 //----------------------------------
 // Anything out
-# resource "aws_security_group_rule" "ingestion_ecs_service_egress" {
-#   type              = "egress"
-#   from_port         = 0
-#   to_port           = 0
-#   protocol          = "-1"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = "${aws_security_group.ingestion_ecs_service.id}"
-# }
+resource "aws_security_group_rule" "ingestion_ecs_service_egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.ingestion_ecs_service.id}"
+}
 
 //--------------------------------------
 // ingestion ECS Service Task level config
