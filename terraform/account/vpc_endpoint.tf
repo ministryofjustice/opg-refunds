b16094695df28e360987dd0b521ab9631f26ac80
Neil Smith <github@nsmith.me.uk>
2019-09-12 20:55:38 +0100

    Added tags

--- terraform/account/vpc_endpoint.tf
+++ terraform/account/vpc_endpoint.tf
@@ -8,11 +8,12 @@ data "aws_vpc_endpoint_service" "dynamodb" {
 
 resource "aws_vpc_endpoint" "dynamodb" {
   vpc_id = aws_default_vpc.default.id
-  #service_name = "com.amazonaws.eu-west-1.dynamodb"
   service_name      = data.aws_vpc_endpoint_service.dynamodb.service_name
   vpc_endpoint_type = "Gateway"
 
   route_table_ids = aws_route_table.private.*.id
+
+  tags        = local.default_tags
 }
 
 //-----------------------------------------
@@ -24,7 +25,6 @@ data "aws_vpc_endpoint_service" "kms" {
 
 resource "aws_vpc_endpoint" "kms" {
   vpc_id = aws_default_vpc.default.id
-  #service_name = "com.amazonaws.eu-west-1.kms"
   service_name      = data.aws_vpc_endpoint_service.kms.service_name
   vpc_endpoint_type = "Interface"
 
@@ -32,6 +32,8 @@ resource "aws_vpc_endpoint" "kms" {
   subnet_ids         = aws_subnet.private.*.id
 
   private_dns_enabled = true
+
+  tags        = local.default_tags
 }
 
 resource "aws_security_group" "kms_vpc_endpoint_access" {
@@ -44,4 +46,6 @@ resource "aws_security_group" "kms_vpc_endpoint_access" {
     protocol  = "-1"
     self      = true
   }
+
+  tags        = local.default_tags
 }
