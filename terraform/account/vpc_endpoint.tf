2c9e12ce31c84131074c6020b647dcd6c6874458
Neil Smith <github@nsmith.me.uk>
2019-09-12 20:51:44 +0100

    Added correct property

--- terraform/account/vpc_endpoint.tf
+++ terraform/account/vpc_endpoint.tf
@@ -9,7 +9,7 @@ data "aws_vpc_endpoint_service" "dynamodb" {
 resource "aws_vpc_endpoint" "dynamodb" {
   vpc_id = aws_default_vpc.default.id
   #service_name = "com.amazonaws.eu-west-1.dynamodb"
-  service_name      = data.aws_vpc_endpoint_service.dynamodb
+  service_name      = data.aws_vpc_endpoint_service.dynamodb.service_name
   vpc_endpoint_type = "Gateway"
 
   route_table_ids = aws_route_table.private.*.id
@@ -25,7 +25,7 @@ data "aws_vpc_endpoint_service" "kms" {
 resource "aws_vpc_endpoint" "kms" {
   vpc_id = aws_default_vpc.default.id
   #service_name = "com.amazonaws.eu-west-1.kms"
-  service_name      = data.aws_vpc_endpoint_service.kms
+  service_name      = data.aws_vpc_endpoint_service.kms.service_name
   vpc_endpoint_type = "Interface"
 
   security_group_ids = [aws_security_group.kms_vpc_endpoint_access.id]
