e051b9a86d9b931db2b2bb2c04b5709b0b6bf652
Neil Smith <github@nsmith.me.uk>
2019-09-13 10:05:08 +0100

    Test policy
    By disabling endpoints. Should fail.

--- terraform/account/vpc_endpoints.tf
+++ terraform/account/vpc_endpoints.tf
@@ -24,42 +24,42 @@ resource "aws_iam_policy" "enforce_vpc_endpoint_access" {
 //-----------------------------------------
 // DynamoDB Endpoint
 
-data "aws_vpc_endpoint_service" "dynamodb" {
-  service = "dynamodb"
-}
-
-resource "aws_vpc_endpoint" "dynamodb" {
-  vpc_id            = aws_default_vpc.default.id
-  service_name      = data.aws_vpc_endpoint_service.dynamodb.service_name
-  vpc_endpoint_type = "Gateway"
-
-  route_table_ids = aws_route_table.private.*.id
-
-  tags = merge(
-    local.default_tags,
-    map("Name", "DynamoDB Gateway")
-  )
-}
-
-//-----------------------------------------
-// KMS Endpoint
-
-data "aws_vpc_endpoint_service" "kms" {
-  service = "kms"
-}
-
-resource "aws_vpc_endpoint" "kms" {
-  vpc_id            = aws_default_vpc.default.id
-  service_name      = data.aws_vpc_endpoint_service.kms.service_name
-  vpc_endpoint_type = "Interface"
-
-  security_group_ids = [aws_security_group.kms_vpc_endpoint.id]
-  subnet_ids         = aws_subnet.private.*.id
-
-  private_dns_enabled = true
-
-  tags = local.default_tags
-}
+//data "aws_vpc_endpoint_service" "dynamodb" {
+//  service = "dynamodb"
+//}
+//
+//resource "aws_vpc_endpoint" "dynamodb" {
+//  vpc_id            = aws_default_vpc.default.id
+//  service_name      = data.aws_vpc_endpoint_service.dynamodb.service_name
+//  vpc_endpoint_type = "Gateway"
+//
+//  route_table_ids = aws_route_table.private.*.id
+//
+//  tags = merge(
+//    local.default_tags,
+//    map("Name", "DynamoDB Gateway")
+//  )
+//}
+//
+////-----------------------------------------
+//// KMS Endpoint
+//
+//data "aws_vpc_endpoint_service" "kms" {
+//  service = "kms"
+//}
+//
+//resource "aws_vpc_endpoint" "kms" {
+//  vpc_id            = aws_default_vpc.default.id
+//  service_name      = data.aws_vpc_endpoint_service.kms.service_name
+//  vpc_endpoint_type = "Interface"
+//
+//  security_group_ids = [aws_security_group.kms_vpc_endpoint.id]
+//  subnet_ids         = aws_subnet.private.*.id
+//
+//  private_dns_enabled = true
+//
+//  tags = local.default_tags
+//}
 
 resource "aws_security_group" "kms_vpc_endpoint" {
   name   = "kms-vpc-endpoint"
