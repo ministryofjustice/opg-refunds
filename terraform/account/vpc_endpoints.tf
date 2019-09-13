a4cd41769326c1f9419b49ef63bfd2cf4460c511
Neil Smith <github@nsmith.me.uk>
2019-09-13 11:10:48 +0100

    Restrict to private subnets

--- terraform/account/vpc_endpoints.tf
+++ terraform/account/vpc_endpoints.tf
@@ -72,7 +72,7 @@ resource "aws_security_group" "kms_vpc_endpoint" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [aws_default_vpc.default.cidr_block]
+    cidr_blocks = aws_subnet.private.*.cidr_block
   }
 
   tags = local.default_tags
