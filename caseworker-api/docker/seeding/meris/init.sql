38172495c72c1b56ecc77076613702c2f8a6b289
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-10-01 14:11:54 +0100

    don't drop tables create and index them if the don't exist and aren't indexed

--- caseworker-api/docker/seeding/meris/init.sql
+++ caseworker-api/docker/seeding/meris/init.sql
@@ -1,12 +1,10 @@
 \c cases
 
-DROP TABLE IF EXISTS meris;
-
-CREATE TABLE meris (
+CREATE TABLE IF NOT EXISTS meris (
   case_number     bigint  NOT NULL,
   sequence_number smallint  NOT NULL,
   data  jsonb   NOT NULL,
   CONSTRAINT meris_pkey PRIMARY KEY (case_number, sequence_number)
 );
 
-CREATE INDEX donor_dob_meris ON meris((data->>'donor-dob'));
+CREATE INDEX IF NOT EXISTS donor_dob_meris ON meris((data->>'donor-dob'));
