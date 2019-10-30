fedde1c829dd263ac8d86b38acc40c84d4acc48d
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-10-30 17:24:25 +0000

    create s3 site for maintenance in new dev account

---
+++ terraform/maintenance/credentials.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "s3" {
+    bucket         = "opg.terraform.state"
+    key            = "moj-lpa-refunds-maintenance/terraform.tfstate"
+    encrypt        = true
+    region         = "eu-west-1"
+    role_arn       = "arn:aws:iam::311462405659:role/opg-refunds-ci"
+    dynamodb_table = "remote_lock"
+  }
+}
+
+variable "default_role" {
+  default = "opg-refunds-ci"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::936779158973:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "eu_central_1"
+  region = "eu-central-1"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::936779158973:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
