5f55dd9631f7888253f34196316a87e348fdf54a
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-10-31 16:18:49 +0000

    provide credentials for old refunds prod account

--- terraform/maintenance/credentials.tf
+++ terraform/maintenance/credentials.tf
@@ -13,6 +13,10 @@ variable "default_role" {
   default = "opg-refunds-ci"
 }
 
+variable "old_account_default_role" {
+  default = "ci"
+}
+
 provider "aws" {
   region = "eu-west-1"
   # moj-refunds-development
@@ -30,6 +34,17 @@ provider "aws" {
 
   assume_role {
     role_arn     = "arn:aws:iam::936779158973:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "old_refunds_production"
+  region = "eu-west-1"
+  # old-refunds-production
+
+  assume_role {
+    role_arn     = "arn:aws:iam::574983609246:role/${var.old_account_default_role}"
     session_name = "terraform-session"
   }
 }
