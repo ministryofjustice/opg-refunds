8faed7d24b6c6e7d1dae9dbbbf14baad3d29a92e
carinadigital <602255+carinadigital@users.noreply.github.com>
2019-11-11 10:22:35 +0000

    LPA-3461: Explicit dependency not needed.

--- terraform/environment/ecs_caseworker_front.tf
+++ terraform/environment/ecs_caseworker_front.tf
@@ -23,8 +23,7 @@ resource "aws_ecs_service" "caseworker_front" {
     container_port   = 80
   }
 
-  depends_on = [aws_rds_cluster.applications, aws_lb.caseworker_front, aws_iam_role.caseworker_front_task_role, aws_iam_role.execution_role]
-  tags       = local.default_tags
+  tags = local.default_tags
 }
 
 //----------------------------------
