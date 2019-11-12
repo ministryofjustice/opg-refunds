e2b22fe900072dfb71a73b14010c06a34fc39b17
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-11-12 11:57:16 +0000

    remove client sg from cluster

--- terraform/environment/db_cluster_caseworker.tf
+++ terraform/environment/db_cluster_caseworker.tf
@@ -1,6 +1,6 @@
 resource "aws_rds_cluster" "caseworker" {
   cluster_identifier     = "caseworker-${local.environment}"
-  vpc_security_group_ids = [aws_security_group.caseworker_rds_cluster.id, aws_security_group.caseworker_rds_cluster_client.id]
+  vpc_security_group_ids = [aws_security_group.caseworker_rds_cluster.id]
   db_subnet_group_name   = aws_db_subnet_group.caseworker_rds_cluster.name
 
   engine                          = "aurora-postgresql"
