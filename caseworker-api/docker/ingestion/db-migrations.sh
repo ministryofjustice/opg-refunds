428e2184280d9c5d5f32d15c58af89beadedace8
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-11-18 13:16:51 +0000

    create tables after doctrine migrations

--- caseworker-api/docker/ingestion/db-migrations.sh
+++ caseworker-api/docker/ingestion/db-migrations.sh
@@ -24,13 +24,14 @@ while : ; do
         psql -h ${OPG_REFUNDS_DB_FINANCE_HOSTNAME} -p ${OPG_REFUNDS_DB_FINANCE_PORT} -U ${POSTGRES_USER} -d postgres -f /app/scripts/init-db-finance.sql
         psql -h ${OPG_REFUNDS_DB_MERIS_HOSTNAME} -p ${OPG_REFUNDS_DB_MERIS_PORT} -U ${POSTGRES_USER} -d postgres -f /app/scripts/init-db-meris.sql
         psql -h ${OPG_REFUNDS_DB_SIRIUS_HOSTNAME} -p ${OPG_REFUNDS_DB_SIRIUS_PORT} -U ${POSTGRES_USER} -d postgres -f /app/scripts/init-db-sirius.sql
+
+        /app/scripts/doctrine-migrations.sh Cases 'migrations:migrate --no-interaction -vvv'
+        /app/scripts/doctrine-migrations.sh Sirius 'migrations:migrate --no-interaction -vvv'
+
         # Create Tables
         psql -h ${OPG_REFUNDS_DB_FINANCE_HOSTNAME} -p ${OPG_REFUNDS_DB_FINANCE_PORT} -U ${POSTGRES_USER} -d postgres -f /app/scripts/init-tables-finance.sql
         psql -h ${OPG_REFUNDS_DB_MERIS_HOSTNAME} -p ${OPG_REFUNDS_DB_MERIS_PORT} -U ${POSTGRES_USER} -d postgres -f /app/scripts/init-tables-meris.sql
         psql -h ${OPG_REFUNDS_DB_SIRIUS_HOSTNAME} -p ${OPG_REFUNDS_DB_SIRIUS_PORT} -U ${POSTGRES_USER} -d postgres -f /app/scripts/init-tables-sirius.sql
-
-        /app/scripts/doctrine-migrations.sh Cases 'migrations:migrate --no-interaction -vvv'
-        /app/scripts/doctrine-migrations.sh Sirius 'migrations:migrate --no-interaction -vvv'
         break
     elif [ $retval -eq 1 ]; then
         # Lock not acquired
