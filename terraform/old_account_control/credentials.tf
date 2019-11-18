1ee8e3242f3d26b681836a83c4f5b62593a00f2c
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-11-18 11:15:55 +0000

    remove unused creedentials

--- terraform/old_account_control/credentials.tf
+++ terraform/old_account_control/credentials.tf
@@ -9,42 +9,16 @@ terraform {
   }
 }
 
-variable "default_role" {
-  default = "opg-refunds-ci"
-}
-
 variable "old_account_default_role" {
   default = "ci"
 }
 
 provider "aws" {
   region = "eu-west-1"
-  # moj-refunds-development
+  # old-refunds-development
 
   assume_role {
-    role_arn     = "arn:aws:iam::936779158973:role/${var.default_role}"
-    session_name = "terraform-session"
-  }
-}
-
-provider "aws" {
-  alias  = "eu_central_1"
-  region = "eu-central-1"
-  # moj-refunds-development
-
-  assume_role {
-    role_arn     = "arn:aws:iam::936779158973:role/${var.default_role}"
-    session_name = "terraform-session"
-  }
-}
-
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-  # moj-refunds-development
-
-  assume_role {
-    role_arn     = "arn:aws:iam::936779158973:role/${var.default_role}"
+    role_arn     = "arn:aws:iam::792093328875:role/${var.old_account_default_role}"
     session_name = "terraform-session"
   }
 }
@@ -56,17 +30,6 @@ provider "aws" {
 
   assume_role {
     role_arn     = "arn:aws:iam::574983609246:role/${var.old_account_default_role}"
-    session_name = "terraform-session"
-  }
-}
-
-provider "aws" {
-  alias  = "old_refunds_development"
-  region = "eu-west-1"
-  # old-refunds-development
-
-  assume_role {
-    role_arn     = "arn:aws:iam::792093328875:role/${var.old_account_default_role}"
     session_name = "terraform-session"
   }
 }
