5a1c3a0b8429a712fa69a63558795c67cd94887b
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-11-21 17:59:36 +0000

    merge in teerraform config from eecs move branch

--- terraform/environment/db_cluster_applications.tf
+++ terraform/environment/db_cluster_applications.tf
@@ -1,12 +1,12 @@
 resource "aws_rds_cluster" "applications" {
   cluster_identifier     = "applications-${local.environment}"
   vpc_security_group_ids = [aws_security_group.applications_rds_cluster.id]
-  db_subnet_group_name   = aws_db_subnet_group.applications_rds_cluster.name
+  db_subnet_group_name   = "${aws_db_subnet_group.applications_rds_cluster.name}"
 
   engine                          = "aurora-postgresql"
-  engine_version                  = "9.6.11"
-  db_cluster_parameter_group_name = "default.aurora-postgresql9.6"
-  database_name                   = "applications"
+  engine_mode                     = "serverless"
+  engine_version                  = "10.7"
+  db_cluster_parameter_group_name = "default.aurora-postgresql10"
 
   backtrack_window  = 0
   apply_immediately = true
@@ -15,26 +15,25 @@ resource "aws_rds_cluster" "applications" {
   master_username = local.rds_master_username
   master_password = data.aws_secretsmanager_secret_version.postgres_password.secret_string
 
-  deletion_protection             = local.account.database_deletion_protection
+  deletion_protection             = local.account.aurora_serverless_deletion_protection
   enabled_cloudwatch_logs_exports = []
   backup_retention_period         = 7
   preferred_backup_window         = "00:14-00:44"
   preferred_maintenance_window    = "wed:22:26-wed:22:56"
   skip_final_snapshot             = true
-  tags                            = local.default_tags
-}
 
-resource "aws_rds_cluster_instance" "applications_cluster_instances" {
-  count              = 1
-  identifier         = "applications-${count.index}"
-  cluster_identifier = aws_rds_cluster.applications.id
-  instance_class     = "db.r4.large"
-  engine             = aws_rds_cluster.applications.engine
-  engine_version     = aws_rds_cluster.applications.engine_version
+  scaling_configuration {
+    auto_pause               = local.account.aurora_serverless_auto_pause
+    max_capacity             = 384
+    min_capacity             = 2
+    seconds_until_auto_pause = 300
+    timeout_action           = "RollbackCapacityChange"
+  }
+  tags = local.default_tags
 }
 
 resource "aws_db_subnet_group" "applications_rds_cluster" {
-  name       = "applications-db-cluster-${local.environment}"
+  name       = "applications-${local.environment}"
   subnet_ids = data.aws_subnet_ids.private.ids
 
   tags = local.default_tags
@@ -42,16 +41,16 @@ resource "aws_db_subnet_group" "applications_rds_cluster" {
 
 
 resource "aws_security_group" "applications_rds_cluster_client" {
-  name                   = "${local.environment}-applications-rds-cluster-client"
-  description            = "client access to applications db cluster"
+  name                   = "applications-rds-cluster-client-${local.environment}"
+  description            = "rds access for ${local.environment}"
   vpc_id                 = data.aws_vpc.default.id
   revoke_rules_on_delete = true
   tags                   = local.default_tags
 }
 
 resource "aws_security_group" "applications_rds_cluster" {
-  name                   = "${local.environment}-applications-rds-cluster"
-  description            = "applications db cluster access"
+  name                   = "rds-cluster-applications-${local.environment}"
+  description            = "api rds access"
   vpc_id                 = data.aws_vpc.default.id
   revoke_rules_on_delete = true
   tags                   = local.default_tags
--- terraform/environment/db_cluster_caseworker.tf
+++ terraform/environment/db_cluster_caseworker.tf
@@ -1,12 +1,12 @@
 resource "aws_rds_cluster" "caseworker" {
   cluster_identifier     = "caseworker-${local.environment}"
-  vpc_security_group_ids = [aws_security_group.caseworker_rds_cluster.id]
-  db_subnet_group_name   = aws_db_subnet_group.caseworker_rds_cluster.name
+  vpc_security_group_ids = [aws_security_group.caseworker_rds_cluster.id, aws_security_group.caseworker_rds_cluster_client.id]
+  db_subnet_group_name   = "${aws_db_subnet_group.caseworker_rds_cluster.name}"
 
   engine                          = "aurora-postgresql"
-  engine_version                  = "9.6.11"
-  db_cluster_parameter_group_name = "default.aurora-postgresql9.6"
-  database_name                   = "caseworker"
+  engine_mode                     = "serverless"
+  engine_version                  = "10.7"
+  db_cluster_parameter_group_name = "default.aurora-postgresql10"
 
   backtrack_window  = 0
   apply_immediately = true
@@ -15,26 +15,25 @@ resource "aws_rds_cluster" "caseworker" {
   master_username = local.rds_master_username
   master_password = data.aws_secretsmanager_secret_version.postgres_password.secret_string
 
-  deletion_protection             = local.account.database_deletion_protection
+  deletion_protection             = local.account.aurora_serverless_deletion_protection
   enabled_cloudwatch_logs_exports = []
   backup_retention_period         = 7
   preferred_backup_window         = "00:14-00:44"
   preferred_maintenance_window    = "wed:22:26-wed:22:56"
   skip_final_snapshot             = true
-  tags                            = local.default_tags
-}
 
-resource "aws_rds_cluster_instance" "caseworker_cluster_instances" {
-  count              = 1
-  identifier         = "caseworker-${count.index}"
-  cluster_identifier = aws_rds_cluster.caseworker.id
-  instance_class     = "db.r4.large"
-  engine             = aws_rds_cluster.caseworker.engine
-  engine_version     = aws_rds_cluster.caseworker.engine_version
+  scaling_configuration {
+    auto_pause               = local.account.aurora_serverless_auto_pause
+    max_capacity             = 384
+    min_capacity             = 2
+    seconds_until_auto_pause = 300
+    timeout_action           = "RollbackCapacityChange"
+  }
+  tags = local.default_tags
 }
 
 resource "aws_db_subnet_group" "caseworker_rds_cluster" {
-  name       = "caseworker-db-cluster-${local.environment}"
+  name       = "caseworker-${local.environment}"
   subnet_ids = data.aws_subnet_ids.private.ids
 
   tags = local.default_tags
@@ -42,16 +41,16 @@ resource "aws_db_subnet_group" "caseworker_rds_cluster" {
 
 
 resource "aws_security_group" "caseworker_rds_cluster_client" {
-  name                   = "${local.environment}-caseworker-rds-cluster-client"
-  description            = "client access to caseworker db cluster"
+  name                   = "caseworker-rds-cluster-client-${local.environment}"
+  description            = "rds access for ${local.environment}"
   vpc_id                 = data.aws_vpc.default.id
   revoke_rules_on_delete = true
   tags                   = local.default_tags
 }
 
 resource "aws_security_group" "caseworker_rds_cluster" {
-  name                   = "${local.environment}-caseworker-rds-cluster"
-  description            = "caseworker db cluster access"
+  name                   = "rds-cluster-caseworker-${local.environment}"
+  description            = "api rds access"
   vpc_id                 = data.aws_vpc.default.id
   revoke_rules_on_delete = true
   tags                   = local.default_tags
