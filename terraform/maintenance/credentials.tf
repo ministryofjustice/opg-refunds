33cebb1bf97c3b8bd197d91b08f0ea75bb1d4436
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-11-21 16:29:38 +0000

    take control of old dns records - not applied

---
+++ terraform/maintenance/credentials.tf
@@ -0,0 +1,73 @@
+terraform {
+  backend "s3" {
+    bucket         = "opg.terraform.state"
+    key            = "moj-lpa-refunds-maintenance/terraform.tfstate"
+    encrypt        = true
+    region         = "eu-west-1"
+    role_arn       = "arn:aws:iam::311462405659:role/opg-refunds-ci"
+    dynamodb_table = "remote_lock"
+  }
+}
+
+variable "default_role" {
+  default = "opg-refunds-ci"
+}
+
+variable "old_account_default_role" {
+  default = "ci"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+  # moj-refunds-development
+
+  assume_role {
+    role_arn     = "arn:aws:iam::936779158973:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "old_refunds_production"
+  region = "eu-west-1"
+  # old-refunds-production
+
+  assume_role {
+    role_arn     = "arn:aws:iam::574983609246:role/${var.old_account_default_role}"
+    session_name = "terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "new_refunds_production"
+  region = "eu-west-1"
+  # new-refunds-production
+
+  assume_role {
+    role_arn     = "arn:aws:iam::805626386523:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
+
+provider "aws" {
+  alias  = "management"
+  region = "eu-west-1"
+  # new-refunds-production
+
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
+
+
+provider "aws" {
+  alias  = "eu_central_1"
+  region = "eu-central-1"
+  # moj-refunds-production
+
+  assume_role {
+    role_arn     = "arn:aws:iam::311462405659:role/${var.default_role}"
+    session_name = "terraform-session"
+  }
+}
