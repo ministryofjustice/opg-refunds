247c99dd0b9904552f5eb592316cf1b548e89306
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-05 14:14:31 +0000

    Use refunds hostd zon and reeduce tfvars required

--- terraform/account/locals.tf
+++ terraform/account/locals.tf
@@ -6,14 +6,10 @@ variable "account_mapping" {
 variable "accounts" {
   type = map(
     object({
-      account_id                               = string
-      is_production                            = bool
-      has_cloudfront_distribution              = bool
-      public_front_certificate_domain_name     = string
-      caseworker_front_certificate_domain_name = string
-      public_front_dns                         = string
-      caseworker_front_dns                     = string
-      old_refunds_account_id                   = string
+      account_id                  = string
+      is_production               = bool
+      has_cloudfront_distribution = bool
+      old_refunds_account_id      = string
     })
   )
 }
