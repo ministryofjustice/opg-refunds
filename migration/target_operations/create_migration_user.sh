b59421b3531332c5fcfd6540e609bdf293dcc4fe
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 15:40:19 +0000

    split create and apply grants

--- migration/target_operations/create_migration_user.sh
+++ migration/target_operations/create_migration_user.sh
@@ -6,3 +6,4 @@ export CASES_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier 
 export OPG_REFUNDS_DB_MIGRATION_PASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_migration_password | jq -r .'SecretString')
 
 PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U root cases < /mnt/opg-refunds/migration/target_operations/create_migration_user.sql
+PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U cases_migration cases < /mnt/opg-refunds/migration/target_operations/apply_grants_migration_user.sql
