c8d609e222a34f18ff826939d0fecb46a565e839
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 17:41:03 +0000

    dump with inserts instead of copies

--- migration/target_operations/load_sql.sh
+++ migration/target_operations/load_sql.sh
@@ -15,6 +15,6 @@ PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refun
 PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U root cases < /mnt/opg-refunds/migration/target_operations/claim_truncate_tables_drop_index_root.sql
 
 # load
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_password | jq -r .'SecretString') pg_restore --data-only --exit-on-error --inserts --table=finance --table=meris --table=sirius -h $CASES_DB_ENDPOINT -U root /mnt/sql/cases.tar
+PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_password | jq -r .'SecretString') pg_restore --data-only --exit-on-error --table=finance --table=meris --table=sirius -h $CASES_DB_ENDPOINT -U root /mnt/sql/cases.tar
 
-# PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') pg_restore --data-only --exit-on-error --inserts --table=claim --table=doctrine_migration_versions --table=duplicate_claims --table=note --table=payment --table=poa --table=report --tabscle=user --table=verification -h $CASES_DB_ENDPOINT -U cases_migration /mnt/sql/cases.tar
+# PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') pg_restore --data-only --exit-on-error --table=claim --table=doctrine_migration_versions --table=duplicate_claims --table=note --table=payment --table=poa --table=report --tabscle=user --table=verification -h $CASES_DB_ENDPOINT -U cases_migration /mnt/sql/cases.tar
