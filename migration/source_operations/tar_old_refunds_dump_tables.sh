53379dd3065d30c50264369e1c245a4e72c0e128
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 09:48:17 +0000

    target operations for cases db

---
+++ migration/source_operations/tar_old_refunds_dump_tables.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Find DB PASSWORDs and enter below
+# grep database_master_password opg-refund-deploy/ansible/production/env_vars.yml
+APP_DB_PASS=
+CASES_DB_PASS=
+
+# grep caseworker_logical_dbs -a6 opg-refund-deploy/ansible/production/env_vars.yml | grep sirius -a2 -b1| grep full_password
+SIRIUS_DB_PASS=
+
+# Applications DBs
+echo "applications"
+export PGPASSWORD=${APP_DB_PASS}
+pg_dump --data-only -h applications.preprod.internal -U refunds_master_full --file=applications.tar --format=t applications
+
+#Caseworker DBs
+
+#     Name    |          Owner          | Encoding |   Collate   |    Ctype    |                  Access privileges                  
+# ------------+-------------------------+----------+-------------+-------------+-----------------------------------------------------
+# cases      | refunds_caseworker_full | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =Tc/refunds_caseworker_full                        +
+#            |                         |          |             |             | refunds_caseworker_full=CTc/refunds_caseworker_full+
+#            |                         |          |             |             | cases_full=c/refunds_caseworker_full               +
+#            |                         |          |             |             | cases_migration=CTc/refunds_caseworker_full
+# caseworker | refunds_caseworker_full | UTF8     | en_US.UTF-8 | en_US.UTF-8 | 
+# finance    | refunds_caseworker_full | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =Tc/refunds_caseworker_full                        +
+#            |                         |          |             |             | refunds_caseworker_full=CTc/refunds_caseworker_full+
+#            |                         |          |             |             | finance_full=c/refunds_caseworker_full             +
+#            |                         |          |             |             | finance_migration=CTc/refunds_caseworker_full
+# meris      | refunds_caseworker_full | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =Tc/refunds_caseworker_full                        +
+#            |                         |          |             |             | refunds_caseworker_full=CTc/refunds_caseworker_full+
+#            |                         |          |             |             | meris_full=c/refunds_caseworker_full               +
+#            |                         |          |             |             | meris_migration=CTc/refunds_caseworker_full
+# postgres   | refunds_caseworker_full | UTF8     | en_US.UTF-8 | en_US.UTF-8 | 
+# rdsadmin   | rdsadmin                | UTF8     | en_US.UTF-8 | en_US.UTF-8 | rdsadmin=CTc/rdsadmin
+# sirius     | refunds_caseworker_full | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =Tc/refunds_caseworker_full                        +
+#            |                         |          |             |             | refunds_caseworker_full=CTc/refunds_caseworker_full+
+#            |                         |          |             |             | sirius_full=c/refunds_caseworker_full              +
+#            |                         |          |             |             | sirius_migration=CTc/refunds_caseworker_full
+# template0  | rdsadmin                | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =c/rdsadmin                                        +
+#            |                         |          |             |             | rdsadmin=CTc/rdsadmin
+# template1  | refunds_caseworker_full | UTF8     | en_US.UTF-8 | en_US.UTF-8 | =c/refunds_caseworker_full                         +
+#            |                         |          |             |             | refunds_caseworker_full=CTc/refunds_caseworker_full
+
+#dump_cmd='pg_dump --column-inserts --data-only -h caseworker.preprod.internal '
+dump_cmd='pg_dump --data-only -h caseworker.preprod.internal '
+
+export PGPASSWORD=${CASES_DB_PASS}
+echo "cases"
+$dump_cmd -U cases_full --format=t --file=cases.tar cases
+echo "caseworker"
+$dump_cmd -U cases_full --format=t --file=caseworker.tar caseworker
+echo "finance"
+$dump_cmd -U cases_full --format=t --file=finance.tar finance
+echo "meris"
+$dump_cmd -U cases_full --format=t --file=meris.tar meris
+
+
+export PGPASSWORD=${SIRIUS_DB_PASS}
+echo "sirius"
+$dump_cmd -U sirius_full --format=t --file=sirius.tar sirius
---
+++ migration/target_operations/claim_truncate_tables_drop_index_root.sql
@@ -0,0 +1,12 @@
+
+# root
+TRUNCATE TABLE finance;
+TRUNCATE TABLE meris;
+TRUNCATE TABLE sirius;
+
+# root
+DROP INDEX donor_dob_meris;
+DROP INDEX donor_dob_sirius;
+DROP INDEX finance_pkey;
+DROP INDEX meris_pkey;
+DROP INDEX sirius_pkey;
