754c72eba0618e3f2876db495b7b88fb5edb527c
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 14:39:34 +0000

    try load with cases_migration

--- migration/target_operations/load_sql.sh
+++ migration/target_operations/load_sql.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_full_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U cases_full cases < /mnt/sql/cases.sql
+PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U cases_migration cases < /mnt/sql/cases.sql
