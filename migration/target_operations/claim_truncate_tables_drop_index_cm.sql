ea9fc9f760d521134e3df202ef85b4f309fbed67
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 10:34:22 +0000

    public user

--- migration/target_operations/claim_truncate_tables_drop_index_cm.sql
+++ migration/target_operations/claim_truncate_tables_drop_index_cm.sql
@@ -8,15 +8,15 @@ TRUNCATE TABLE public.report;
 TRUNCATE TABLE public.user;
 TRUNCATE TABLE public.verification;
 
-ALTER TABLE claim DROP CONSTRAINT IF EXISTS claim_pkey;
-ALTER TABLE doctrine_migration_versions DROP CONSTRAINT IF EXISTS doctrine_migration_versions_pkey;
-ALTER TABLE duplicate_claims DROP CONSTRAINT IF EXISTS duplicate_claims_pkey;
-ALTER TABLE note DROP CONSTRAINT IF EXISTS log_pkey;
-ALTER TABLE payment DROP CONSTRAINT IF EXISTS payment_pkey;
-ALTER TABLE poa DROP CONSTRAINT IF EXISTS poa_pkey;
-ALTER TABLE report DROP CONSTRAINT IF EXISTS report_pkey;
-ALTER TABLE user DROP CONSTRAINT IF EXISTS user_pkey;
-ALTER TABLE verification DROP CONSTRAINT IF EXISTS verification_pkey;
+ALTER TABLE public.claim DROP CONSTRAINT IF EXISTS claim_pkey;
+ALTER TABLE public.doctrine_migration_versions DROP CONSTRAINT IF EXISTS doctrine_migration_versions_pkey;
+ALTER TABLE public.duplicate_claims DROP CONSTRAINT IF EXISTS duplicate_claims_pkey;
+ALTER TABLE public.note DROP CONSTRAINT IF EXISTS log_pkey;
+ALTER TABLE public.payment DROP CONSTRAINT IF EXISTS payment_pkey;
+ALTER TABLE public.poa DROP CONSTRAINT IF EXISTS poa_pkey;
+ALTER TABLE public.report DROP CONSTRAINT IF EXISTS report_pkey;
+ALTER TABLE public.user DROP CONSTRAINT IF EXISTS user_pkey;
+ALTER TABLE public.verification DROP CONSTRAINT IF EXISTS verification_pkey;
 
 DROP INDEX IF EXISTS claim_pkey;
 DROP INDEX IF EXISTS doctrine_migration_versions_pkey;
