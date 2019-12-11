f8b324450efba0e164bdd4a0211e19edee7fb1ad
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 09:57:17 +0000

    aliases

---
+++ migration/instance_config/alias.sh
@@ -0,0 +1,7 @@
+alias l="ls -al"
+alias gco="git checkout"
+alias ga="git add"
+alias gc="git commit"
+alias gst="git status"
+alias ..="cd .."
+alias concases="PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U cases_migration cases"
