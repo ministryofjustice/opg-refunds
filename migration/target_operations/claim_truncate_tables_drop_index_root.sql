7812048efb62659df2871b7c56154b5c3b1a4a6e
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 10:34:22 +0000

    public user

--- migration/target_operations/claim_truncate_tables_drop_index_root.sql
+++ migration/target_operations/claim_truncate_tables_drop_index_root.sql
@@ -2,11 +2,11 @@ TRUNCATE TABLE public.finance;
 TRUNCATE TABLE public.meris;
 TRUNCATE TABLE public.sirius;
 
-ALTER TABLE meris DROP CONSTRAINT IF EXISTS donor_dob_meris;
-ALTER TABLE sirius DROP CONSTRAINT IF EXISTS donor_dob_sirius;
-ALTER TABLE finance DROP CONSTRAINT IF EXISTS finance_pkey;
-ALTER TABLE meris DROP CONSTRAINT IF EXISTS meris_pkey;
-ALTER TABLE sirius DROP CONSTRAINT IF EXISTS sirius_pkey;
+ALTER TABLE public.meris DROP CONSTRAINT IF EXISTS donor_dob_meris;
+ALTER TABLE public.sirius DROP CONSTRAINT IF EXISTS donor_dob_sirius;
+ALTER TABLE public.finance DROP CONSTRAINT IF EXISTS finance_pkey;
+ALTER TABLE public.meris DROP CONSTRAINT IF EXISTS meris_pkey;
+ALTER TABLE public.sirius DROP CONSTRAINT IF EXISTS sirius_pkey;
 
 DROP INDEX IF EXISTS donor_dob_meris;
 DROP INDEX IF EXISTS donor_dob_sirius;
