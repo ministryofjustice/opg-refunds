b1e8cb5859234823a1d12afd4ac14840368a0ca8
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 10:20:10 +0000

    configuration for env vars

--- migration/instance_config/env_vars.sh
+++ migration/instance_config/env_vars.sh
@@ -2,6 +2,8 @@
 
 export ENV_NAME=preproduction
 export AWS_DEFAULT_REGION=eu-west-1
+export OPG_REFUNDS_DB_APPLICATIONS_HOSTNAME=$(aws rds describe-db-clusters --db-cluster-identifier applications-$ENV_NAME | jq -r .'DBClusters'[0].'Endpoint')
+export OPG_REFUNDS_DB_APPLICATIONS_PORT=5432
 export CASES_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier caseworker-$ENV_NAME | jq -r .'DBClusters'[0].'Endpoint')
 export OPG_REFUNDS_DB_FINANCE_HOSTNAME=$CASES_DB_ENDPOINT
 export OPG_REFUNDS_DB_FINANCE_PORT=5432
@@ -9,6 +11,4 @@ export OPG_REFUNDS_DB_SIRIUS_HOSTNAME=$CASES_DB_ENDPOINT
 export OPG_REFUNDS_DB_SIRIUS_PORT=5432
 export OPG_REFUNDS_DB_MERIS_HOSTNAME=$CASES_DB_ENDPOINT
 export OPG_REFUNDS_DB_MERIS_PORT=5432
-
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U cases_migration cases < claim_truncate_tables_drop_index_cm.sql
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U root cases < claim_truncate_tables_drop_index_root.sql
+export POSTGRES_USER=root
