2ffe3b456205230eb1272a5933f84ce5fd442525
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 10:29:53 +0000

    drop constraints if they exist

--- migration/target_operations/claim_truncate_tables_drop_index_root.sql
+++ migration/target_operations/claim_truncate_tables_drop_index_root.sql
@@ -2,8 +2,12 @@ TRUNCATE TABLE public.finance;
 TRUNCATE TABLE public.meris;
 TRUNCATE TABLE public.sirius;
 
-DROP INDEX donor_dob_meris;
-DROP INDEX donor_dob_sirius;
-DROP INDEX finance_pkey;
-DROP INDEX meris_pkey;
-DROP INDEX sirius_pkey;
+ALTER TABLE finance DROP CONSTRAINT IF EXISTS finance_pkey;
+ALTER TABLE meris DROP CONSTRAINT IF EXISTS meris_pkey;
+ALTER TABLE sirius DROP CONSTRAINT IF EXISTS sirius_pkey;
+
+DROP INDEX IF EXISTS donor_dob_meris;
+DROP INDEX IF EXISTS donor_dob_sirius;
+DROP INDEX IF EXISTS finance_pkey;
+DROP INDEX IF EXISTS meris_pkey;
+DROP INDEX IF EXISTS sirius_pkey;
