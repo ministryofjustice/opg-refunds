c55dd59fb76411fa62bb6685cb323e2e9568b017
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-11 11:05:37 +0000

    update aliases

--- migration/target_operations/claim_truncate_tables_drop_index_root.sql
+++ migration/target_operations/claim_truncate_tables_drop_index_root.sql
@@ -2,14 +2,14 @@ TRUNCATE TABLE public.finance;
 TRUNCATE TABLE public.meris;
 TRUNCATE TABLE public.sirius;
 
-ALTER TABLE public.meris DROP CONSTRAINT IF EXISTS donor_dob_meris;
-ALTER TABLE public.sirius DROP CONSTRAINT IF EXISTS donor_dob_sirius;
-ALTER TABLE public.finance DROP CONSTRAINT IF EXISTS finance_pkey;
-ALTER TABLE public.meris DROP CONSTRAINT IF EXISTS meris_pkey;
-ALTER TABLE public.sirius DROP CONSTRAINT IF EXISTS sirius_pkey;
+-- ALTER TABLE public.meris DROP CONSTRAINT IF EXISTS donor_dob_meris;
+-- ALTER TABLE public.sirius DROP CONSTRAINT IF EXISTS donor_dob_sirius;
+-- ALTER TABLE public.finance DROP CONSTRAINT IF EXISTS finance_pkey;
+-- ALTER TABLE public.meris DROP CONSTRAINT IF EXISTS meris_pkey;
+-- ALTER TABLE public.sirius DROP CONSTRAINT IF EXISTS sirius_pkey;
 
-DROP INDEX IF EXISTS donor_dob_meris;
-DROP INDEX IF EXISTS donor_dob_sirius;
-DROP INDEX IF EXISTS finance_pkey;
-DROP INDEX IF EXISTS meris_pkey;
-DROP INDEX IF EXISTS sirius_pkey;
+-- DROP INDEX IF EXISTS donor_dob_meris;
+-- DROP INDEX IF EXISTS donor_dob_sirius;
+-- DROP INDEX IF EXISTS finance_pkey;
+-- DROP INDEX IF EXISTS meris_pkey;
+-- DROP INDEX IF EXISTS sirius_pkey;
