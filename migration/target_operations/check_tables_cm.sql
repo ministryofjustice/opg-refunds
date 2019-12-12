6ef6fd3b237192c3185791e0d2d672c45d8d9eb9
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-12 12:23:59 +0000

    improve scripts

--- migration/target_operations/check_tables_cm.sql
+++ migration/target_operations/check_tables_cm.sql
@@ -1,9 +1,10 @@
-SELECT 'claim', COUNT(*) FROM public.claim;
-SELECT 'doctrine_migration_versions', COUNT(*) FROM public.doctrine_migration_versions;
-SELECT 'duplicate_claims', COUNT(*) FROM public.duplicate_claims;
-SELECT 'note', COUNT(*) FROM public.note;
-SELECT 'payment', COUNT(*) FROM public.payment;
-SELECT 'poa', COUNT(*) FROM public.poa;
-SELECT 'report', COUNT(*) FROM public.report;
-SELECT 'user', COUNT(*) FROM public.user;
-SELECT 'verification', COUNT(*) FROM public.verification;
+SELECT 'claim', COUNT(*) FROM public.claim UNION
+SELECT 'doctrine_migration_versions', COUNT(*) FROM public.doctrine_migration_versions UNION
+SELECT 'duplicate_claims', COUNT(*) FROM public.duplicate_claims UNION
+SELECT 'note', COUNT(*) FROM public.note UNION
+SELECT 'payment', COUNT(*) FROM public.payment UNION
+SELECT 'poa', COUNT(*) FROM public.poa UNION
+SELECT 'report', COUNT(*) FROM public.report UNION
+SELECT 'user', COUNT(*) FROM public.user UNION
+SELECT 'verification', COUNT(*) FROM public.verification
+;
