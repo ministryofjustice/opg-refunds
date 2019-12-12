cd18901467c8c9516823ffe625fe13d83a0882c7
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-12 12:26:08 +0000

    run counts for all tables on load

--- migration/target_operations/load_sql.sh
+++ migration/target_operations/load_sql.sh
@@ -22,4 +22,4 @@ PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_
 # check
 PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U root cases < /mnt/opg-refunds/migration/target_operations/check_tables_root.sql
 
-# PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U cases_migration cases < /mnt/opg-refunds/migration/target_operations/check_tables_cm.sql
+PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U cases_migration cases < /mnt/opg-refunds/migration/target_operations/check_tables_cm.sql
