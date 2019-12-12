f808890944277543ee3386bff1f1599a895644b6
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-12 11:57:37 +0000

    add a check for dat in the tables

---
+++ migration/target_operations/check_tables_cm.sql
@@ -0,0 +1,9 @@
+SELECT * FROM public.claim LIMIT 10;
+SELECT * FROM public.doctrine_migration_versions LIMIT 10;
+SELECT * FROM public.duplicate_claims LIMIT 10;
+SELECT * FROM public.note LIMIT 10;
+SELECT * FROM public.payment LIMIT 10;
+SELECT * FROM public.poa LIMIT 10;
+SELECT * FROM public.report LIMIT 10;
+SELECT * FROM public.user LIMIT 10;
+SELECT * FROM public.verification LIMIT 10;
