5f2deaed1c201aa8b1900f8f6ee9db799ea9a56d
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-12 16:36:54 +0000

    refactor script for readability

--- migration/instance_config/env_vars.sh
+++ migration/instance_config/env_vars.sh
@@ -2,13 +2,3 @@
 
 export ENV_NAME=preproduction
 export AWS_DEFAULT_REGION=eu-west-1
-export OPG_REFUNDS_DB_APPLICATIONS_HOSTNAME=$(aws rds describe-db-clusters --db-cluster-identifier applications-$ENV_NAME | jq -r .'DBClusters'[0].'Endpoint')
-export OPG_REFUNDS_DB_APPLICATIONS_PORT=5432
-export CASES_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier caseworker-$ENV_NAME | jq -r .'DBClusters'[0].'Endpoint')
-export OPG_REFUNDS_DB_FINANCE_HOSTNAME=$CASES_DB_ENDPOINT
-export OPG_REFUNDS_DB_FINANCE_PORT=5432
-export OPG_REFUNDS_DB_SIRIUS_HOSTNAME=$CASES_DB_ENDPOINT
-export OPG_REFUNDS_DB_SIRIUS_PORT=5432
-export OPG_REFUNDS_DB_MERIS_HOSTNAME=$CASES_DB_ENDPOINT
-export OPG_REFUNDS_DB_MERIS_PORT=5432
-export POSTGRES_USER=root
--- migration/target_operations/load_sql.sh
+++ migration/target_operations/load_sql.sh
@@ -1,25 +1,54 @@
 #!/usr/bin/env bash
 
-export ENV_NAME=preproduction
-export AWS_DEFAULT_REGION=eu-west-1
-export CASES_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier caseworker-$ENV_NAME | jq -r .'DBClusters'[0].'Endpoint')
-export OPG_REFUNDS_DB_FINANCE_HOSTNAME=$CASES_DB_ENDPOINT
-export OPG_REFUNDS_DB_FINANCE_PORT=5432
-export OPG_REFUNDS_DB_SIRIUS_HOSTNAME=$CASES_DB_ENDPOINT
-export OPG_REFUNDS_DB_SIRIUS_PORT=5432
-export OPG_REFUNDS_DB_MERIS_HOSTNAME=$CASES_DB_ENDPOINT
-export OPG_REFUNDS_DB_MERIS_PORT=5432
+if [[ -z "${ENV_NAME}" ]]; then
+  "environment set as $ENV_VAR"
+else
+  "ENV_NAME not set!"
+  exit 1
+fi
 
-# truncate
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U cases_migration cases < /mnt/opg-refunds/migration/target_operations/claim_truncate_tables_drop_index_cm.sql
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U root cases < /mnt/opg-refunds/migration/target_operations/claim_truncate_tables_drop_index_root.sql
+CASES_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier caseworker-${ENV_NAME} | jq -r .'DBClusters'[0].'Endpoint')
+APPLICATIONS_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier applications-$ENV_NAME | jq -r .'DBClusters'[0].'Endpoint')
+SCRIPTS_PATH="/mnt/opg-refunds/migration/target_operations"
+DATA_PATH="/mnt/sql"
+ROOT_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${ENV_NAME}/postgres_password | jq -r .'SecretString') 
+CASES_MIGRATION_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${ENV_NAME}/opg_refunds_db_cases_migration_password | jq -r .'SecretString')
 
-# load
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_password | jq -r .'SecretString') pg_restore --data-only --exit-on-error --table=finance --table=meris --table=sirius -h $CASES_DB_ENDPOINT -U root --dbname=cases --verbose /mnt/sql/cases.tar
+ROOT_OPTS="-h ${CASES_DB_ENDPOINT} -U root -W ${ROOT_PASSWORD}"
+CASES_MIGRATION_OPTS="-h ${CASES_DB_ENDPOINT} -U cases_migration -W ${CASES_MIGRATION_PASSWORD}"
 
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') pg_restore --data-only --exit-on-error --table=claim --table=doctrine_migration_versions --table=duplicate_claims --table=note --table=payment --table=poa --table=report --table=user --table=verification -h $CASES_DB_ENDPOINT -U cases_migration --dbname=cases /mnt/sql/cases.tar
+PSQL="psql -v ON_ERROR_STOP=1"
+# truncate cases
+${PSQL} ${CASES_MIGRATION_OPTS} cases < ${SCRIPTS_PATH}/cases_truncate_tables_drop_index_cm.sql
+${PSQL} ${ROOT_OPTS} cases < ${SCRIPTS_PATH}/cases_truncate_tables_drop_index_root.sql
 
-# check
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/postgres_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U root cases < /mnt/opg-refunds/migration/target_operations/check_tables_root.sql
+# load cases
+pg_restore ${ROOT_OPTS} \
+    --dbname=cases \
+    --data-only \
+    --table=finance \
+    --table=meris \
+    --table=sirius \
+    --verbose \
+    --exit-on-error \
+    ${DATA_PATH}/cases.tar
 
-PGPASSWORD=$(aws secretsmanager get-secret-value --secret-id $ENV_NAME/opg_refunds_db_cases_migration_password | jq -r .'SecretString') psql -v ON_ERROR_STOP=1 -h $CASES_DB_ENDPOINT -U cases_migration cases < /mnt/opg-refunds/migration/target_operations/check_tables_cm.sql
+pg_restore ${CASES_MIGRATION_OPTS}  \
+    --dbname=cases \
+    --data-only \
+    --table=claim \
+    --table=doctrine_migration_versions \
+    --table=duplicate_claims \
+    --table=note \
+    --table=payment \
+    --table=poa \
+    --table=report \
+    --table=user \
+    --table=verification \
+    --verbose \
+    --exit-on-error \
+    ${DATA_PATH}/cases.tar
+
+# check cases
+${PSQL} ${ROOT_OPTS} cases < ${SCRIPTS_PATH}/check_tables_root.sql
+${PSQL} ${CASES_MIGRATION_OPTS} cases < ${SCRIPTS_PATH}/check_tables_cm.sql
