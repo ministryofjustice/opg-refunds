347e5bd1f1764fd14518d7597f199c825badc646
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-13 14:16:47 +0000

    repairs to old account config

--- terraform/old_account_control/asg_control.tf
+++ terraform/old_account_control/asg_control.tf
@@ -17,7 +17,15 @@ locals {
     caseworker   = 0
   }
   desired = var.state_up ? local.up_state : local.down_state
-  lifecycle = {
+}
+
+resource "aws_autoscaling_group" "public_front" {
+  provider         = aws.old_refunds_production
+  name             = "front-${var.stack}"
+  max_size         = 2
+  min_size         = local.desired.public_front
+  desired_capacity = local.desired.public_front
+  lifecycle {
     ignore_changes = [
       max_size,
       force_delete,
@@ -30,22 +38,23 @@ locals {
   }
 }
 
-resource "aws_autoscaling_group" "public_front" {
-  provider         = aws.old_refunds_production
-  name             = "front-${var.stack}"
-  max_size         = 2
-  min_size         = local.desired.public_front
-  desired_capacity = local.desired.public_front
-  lifecycle        = local.lifecycle
-}
-
 resource "aws_autoscaling_group" "caseworker_front" {
   provider         = aws.old_refunds_production
   name             = "caseworker-front-${var.stack}"
   max_size         = 2
   min_size         = local.desired.caseworker
   desired_capacity = local.desired.caseworker
-  lifecycle        = local.lifecycle
+  lifecycle {
+    ignore_changes = [
+      max_size,
+      force_delete,
+      tag,
+      health_check_grace_period,
+      launch_configuration,
+      termination_policies,
+      wait_for_capacity_timeout,
+    ]
+  }
 }
 
 resource "aws_autoscaling_group" "caseworker_api" {
@@ -54,5 +63,15 @@ resource "aws_autoscaling_group" "caseworker_api" {
   max_size         = 2
   min_size         = local.desired.caseworker
   desired_capacity = local.desired.caseworker
-  lifecycle        = local.lifecycle
+  lifecycle {
+    ignore_changes = [
+      max_size,
+      force_delete,
+      tag,
+      health_check_grace_period,
+      launch_configuration,
+      termination_policies,
+      wait_for_capacity_timeout,
+    ]
+  }
 }
