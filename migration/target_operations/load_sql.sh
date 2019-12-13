48bca9959124e39709d6c8e38176ecbc55f15a65
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-13 11:49:53 +0000

    runn whole migration

--- migration/target_operations/load_sql.sh
+++ migration/target_operations/load_sql.sh
@@ -26,46 +26,46 @@ APPLICATIONS_MIGRATION_OPTS="-h ${APPLICATIONS_DB_ENDPOINT} -U applications_migr
 
 PSQL="psql -v ON_ERROR_STOP=1"
 # truncate
-# PGPASSWORD=${CASES_MIGRATION_PASSWORD} ${PSQL} ${CASES_MIGRATION_OPTS} cases < ${SCRIPTS_PATH}/cases_truncate_tables_cm.sql
-# PGPASSWORD=${ROOT_PASSWORD} ${PSQL} ${CASES_ROOT_OPTS} cases < ${SCRIPTS_PATH}/cases_truncate_tables_root.sql
-# PGPASSWORD=${SIRIUS_MIGRATION_PASSWORD} ${PSQL} ${SIRIUS_MIGRATION_OPTS} sirius < ${SCRIPTS_PATH}/sirius_truncate_tables_sm.sql
+PGPASSWORD=${CASES_MIGRATION_PASSWORD} ${PSQL} ${CASES_MIGRATION_OPTS} cases < ${SCRIPTS_PATH}/cases_truncate_tables_cm.sql
+PGPASSWORD=${ROOT_PASSWORD} ${PSQL} ${CASES_ROOT_OPTS} cases < ${SCRIPTS_PATH}/cases_truncate_tables_root.sql
+PGPASSWORD=${SIRIUS_MIGRATION_PASSWORD} ${PSQL} ${SIRIUS_MIGRATION_OPTS} sirius < ${SCRIPTS_PATH}/sirius_truncate_tables_sm.sql
 PGPASSWORD=${APPLICATIONS_MIGRATION_PASSWORD} ${PSQL} ${APPLICATIONS_MIGRATION_OPTS} applications < ${SCRIPTS_PATH}/applications_truncate_tables_am.sql
 
 # load cases
-# PGPASSWORD=${ROOT_PASSWORD} pg_restore ${CASES_ROOT_OPTS} \
-#     --dbname=cases \
-#     --data-only \
-#     --table=finance \
-#     --table=meris \
-#     --table=sirius \
-#     --verbose \
-#     --exit-on-error \
-#     ${DATA_PATH}/cases.tar
+PGPASSWORD=${ROOT_PASSWORD} pg_restore ${CASES_ROOT_OPTS} \
+    --dbname=cases \
+    --data-only \
+    --table=finance \
+    --table=meris \
+    --table=sirius \
+    --verbose \
+    --exit-on-error \
+    ${DATA_PATH}/cases.tar
 
-# PGPASSWORD=${CASES_MIGRATION_PASSWORD} pg_restore ${CASES_MIGRATION_OPTS}  \
-#     --dbname=cases \
-#     --data-only \
-#     --table=claim \
-#     --table=doctrine_migration_versions \
-#     --table=duplicate_claims \
-#     --table=note \
-#     --table=payment \
-#     --table=poa \
-#     --table=report \
-#     --table=user \
-#     --table=verification \
-#     --verbose \
-#     --exit-on-error \
-#     ${DATA_PATH}/cases.tar
+PGPASSWORD=${CASES_MIGRATION_PASSWORD} pg_restore ${CASES_MIGRATION_OPTS}  \
+    --dbname=cases \
+    --data-only \
+    --table=claim \
+    --table=doctrine_migration_versions \
+    --table=duplicate_claims \
+    --table=note \
+    --table=payment \
+    --table=poa \
+    --table=report \
+    --table=user \
+    --table=verification \
+    --verbose \
+    --exit-on-error \
+    ${DATA_PATH}/cases.tar
 
-# PGPASSWORD=${SIRIUS_MIGRATION_PASSWORD} pg_restore ${SIRIUS_MIGRATION_OPTS}  \
-#     --dbname=sirius \
-#     --data-only \
-#     --table=doctrine_migration_versions \
-#     --table=poa \
-#     --verbose \
-#     --exit-on-error \
-#     ${DATA_PATH}/sirius.tar
+PGPASSWORD=${SIRIUS_MIGRATION_PASSWORD} pg_restore ${SIRIUS_MIGRATION_OPTS}  \
+    --dbname=sirius \
+    --data-only \
+    --table=doctrine_migration_versions \
+    --table=poa \
+    --verbose \
+    --exit-on-error \
+    ${DATA_PATH}/sirius.tar
 
 PGPASSWORD=${APPLICATIONS_MIGRATION_PASSWORD} pg_restore ${APPLICATIONS_MIGRATION_OPTS}  \
     --dbname=applications \
