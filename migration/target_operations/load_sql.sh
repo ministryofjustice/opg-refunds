64832da5fc5bf9764ceeb974bd4f66240f0e6b71
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-13 11:02:07 +0000

    add sirius db to load script

--- migration/target_operations/load_sql.sh
+++ migration/target_operations/load_sql.sh
@@ -8,7 +8,7 @@ else
   echo "environment set as $ENV_NAME"
 fi
 
-CASES_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier caseworker-${ENV_NAME} | jq -r .'DBClusters'[0].'Endpoint')
+CASEWORKER_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier caseworker-${ENV_NAME} | jq -r .'DBClusters'[0].'Endpoint')
 APPLICATIONS_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier applications-${ENV_NAME} | jq -r .'DBClusters'[0].'Endpoint')
 
 SCRIPTS_PATH="/mnt/opg-refunds/migration/target_operations"
@@ -16,14 +16,17 @@ DATA_PATH="/mnt/sql"
 
 ROOT_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${ENV_NAME}/postgres_password | jq -r .'SecretString') 
 CASES_MIGRATION_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${ENV_NAME}/opg_refunds_db_cases_migration_password | jq -r .'SecretString')
+SIRIUS_MIGRATION_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${ENV_NAME}/opg_refunds_db_sirius_migration_password | jq -r .'SecretString')
 
-CASES_ROOT_OPTS="-h ${CASES_DB_ENDPOINT} -U root"
-CASES_MIGRATION_OPTS="-h ${CASES_DB_ENDPOINT} -U cases_migration"
+CASES_ROOT_OPTS="-h ${CASEWORKER_DB_ENDPOINT} -U root"
+CASES_MIGRATION_OPTS="-h ${CASEWORKER_DB_ENDPOINT} -U cases_migration"
+SIRIUS_MIGRATION_OPTS="-h ${CASEWORKER_DB_ENDPOINT} -U sirius_migration"
 
 PSQL="psql -v ON_ERROR_STOP=1"
-# truncate cases
+# truncate
 PGPASSWORD=${CASES_MIGRATION_PASSWORD} ${PSQL} ${CASES_MIGRATION_OPTS} cases < ${SCRIPTS_PATH}/cases_truncate_tables_drop_index_cm.sql
 PGPASSWORD=${ROOT_PASSWORD} ${PSQL} ${CASES_ROOT_OPTS} cases < ${SCRIPTS_PATH}/cases_truncate_tables_drop_index_root.sql
+PGPASSWORD=${SIRIUS_MIGRATION_PASSWORD} ${PSQL} ${SIRIUS_MIGRATION_OPTS} sirius < ${SCRIPTS_PATH}/sirius_truncate_tables_drop_index_sm.sql
 
 # load cases
 PGPASSWORD=${ROOT_PASSWORD} pg_restore ${CASES_ROOT_OPTS} \
@@ -52,6 +55,17 @@ PGPASSWORD=${CASES_MIGRATION_PASSWORD} pg_restore ${CASES_MIGRATION_OPTS}  \
     --exit-on-error \
     ${DATA_PATH}/cases.tar
 
+PGPASSWORD=${SIRIUS_MIGRATION_PASSWORD} pg_restore ${SIRIUS_MIGRATION_OPTS}  \
+    --dbname=sirius \
+    --data-only \
+    --table=doctrine_migration_versions \
+    --table=poa \
+    --table=poa_id_seq \
+    --verbose \
+    --exit-on-error \
+    ${DATA_PATH}/sirius.tar
+
 # check cases
 PGPASSWORD=${ROOT_PASSWORD} ${PSQL} ${CASES_ROOT_OPTS} cases < ${SCRIPTS_PATH}/cases_check_tables_root.sql
 PGPASSWORD=${CASES_MIGRATION_PASSWORD} ${PSQL} ${CASES_MIGRATION_OPTS} cases < ${SCRIPTS_PATH}/cases_check_tables_cm.sql
+PGPASSWORD=${SIRIUS_MIGRATION_PASSWORD} ${PSQL} ${SIRIUS_MIGRATION_OPTS} sirius < ${SCRIPTS_PATH}/sirius_check_tables_cm.sql
