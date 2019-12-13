f0bfea0d70b3f291013dceeb1cc17a0f0f4a60f6
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-13 09:55:41 +0000

    rename files and make connection script

--- migration/source_operations/copy_to_s3.sh
+++ migration/source_operations/copy_to_s3.sh
@@ -2,9 +2,9 @@
 export ENV_NAME=preproduction
 export AWS_DEFAULT_REGION=eu-west-1
 
-aws s3 cp /mnt/sql/applications.sql s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
-aws s3 cp /mnt/sql/cases.sql s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
-aws s3 cp /mnt/sql/caseworker.sql s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
-aws s3 cp /mnt/sql/finance.sql s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
-aws s3 cp /mnt/sql/meris.sql s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
-aws s3 cp /mnt/sql/sirius.sql s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
+aws s3 cp /mnt/sql/applications.tar s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
+aws s3 cp /mnt/sql/cases.tar s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
+aws s3 cp /mnt/sql/caseworker.tar s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
+aws s3 cp /mnt/sql/finance.tar s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
+aws s3 cp /mnt/sql/meris.tar s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
+aws s3 cp /mnt/sql/sirius.tar s3://lpa-refunds-$ENV_NAME-sql-migration/ --sse --acl bucket-owner-full-control
