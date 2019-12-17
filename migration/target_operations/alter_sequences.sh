730aba2158ea38a4c8b6ddf163c2eb54ab1d117d
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-17 17:17:53 +0000

    alter sequences in target to bring up to date

---
+++ migration/target_operations/alter_sequences.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+if [[ -z "$ENV_NAME" ]]; then
+  echo "ENV_NAME not set!"
+  exit 1
+else
+  echo "environment set as $ENV_NAME"
+fi
+
+CASEWORKER_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier caseworker-${ENV_NAME} | jq -r .'DBClusters'[0].'Endpoint')
+APPLICATIONS_DB_ENDPOINT=$(aws rds describe-db-clusters --db-cluster-identifier applications-${ENV_NAME} | jq -r .'DBClusters'[0].'Endpoint')
+
+SCRIPTS_PATH="/mnt/opg-refunds/migration/sql_scripts"
+DATA_PATH="/mnt/sql"
+
+ROOT_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${ENV_NAME}/postgres_password | jq -r .'SecretString') 
+CASES_MIGRATION_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${ENV_NAME}/opg_refunds_db_cases_migration_password | jq -r .'SecretString')
+SIRIUS_MIGRATION_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${ENV_NAME}/opg_refunds_db_sirius_migration_password | jq -r .'SecretString')
+APPLICATIONS_MIGRATION_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${ENV_NAME}/opg_refunds_db_applications_migration_password | jq -r .'SecretString')
+
+CASES_ROOT_OPTS="-h ${CASEWORKER_DB_ENDPOINT} -U root"
+CASES_MIGRATION_OPTS="-h ${CASEWORKER_DB_ENDPOINT} -U cases_migration"
+SIRIUS_MIGRATION_OPTS="-h ${CASEWORKER_DB_ENDPOINT} -U sirius_migration"
+APPLICATIONS_MIGRATION_OPTS="-h ${APPLICATIONS_DB_ENDPOINT} -U applications_migration"
+
+PSQL="psql -v ON_ERROR_STOP=1"
+
+# check cases
+PGPASSWORD=${ROOT_PASSWORD} ${PSQL} ${CASES_ROOT_OPTS} cases < ${SCRIPTS_PATH}/cases_alter_sequence_root.sql
