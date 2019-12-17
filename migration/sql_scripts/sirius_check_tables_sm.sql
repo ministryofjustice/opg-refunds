6e40fb3f5dd8cdbc8323ffc36958ee1b80ec812b
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2019-12-17 16:59:19 +0000

    print the next sequence

--- migration/sql_scripts/sirius_check_tables_sm.sql
+++ migration/sql_scripts/sirius_check_tables_sm.sql
@@ -1,4 +1,3 @@
 SELECT 'doctrine_migration_versions' AS sirius_tables, COUNT(*) FROM public.doctrine_migration_versions UNION
-SELECT 'poa' AS sirius_tables, COUNT(*) FROM public.poa UNION
-SELECT 'poa_id_seq' AS sirius_tables, nextval('public.poa_id_seq')
+SELECT 'poa' AS sirius_tables, COUNT(*) FROM public.poa
 ;
