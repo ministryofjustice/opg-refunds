65bafb61c78207ee27bdb85ad68da666a570da17
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-03-02 11:49:21 +0000

    configure terraform

--- docs/runbooks/maintenance_mode/manage_maintenance.sh
+++ docs/runbooks/maintenance_mode/manage_maintenance.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+function configure_terraform() {
+  export TF_VAR_default_role=${C9_USER}
+  export TF_VAR_old_account_default_role=${C9_USER}
+  export TF_CLI_ARGS_init="-backend-config=role_arn=arn:aws:iam::311462405659:role/${C9_USER} -upgrade=true  -reconfigure"
+}
+
 function enable_maintenance() {
   aws ssm put-parameter --name "${ENVIRONMENT}_enable_maintenance" --type "String" --value "true" --overwrite
   terraform workspace select ${ENVIRONMENT}
@@ -40,4 +46,5 @@ function start() {
 
 MAINTENACE_MODE=False
 parse_args $@
+configure_terraform
 start
