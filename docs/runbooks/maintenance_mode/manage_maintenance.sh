5cf10597d52a7859c2e325334df18c67cf4e8897
andrewpearce-digital <andrew.pearce@digital.justice.gov.uk>
2020-03-02 16:16:23 +0000

    trying to tag variables

--- docs/runbooks/maintenance_mode/manage_maintenance.sh
+++ docs/runbooks/maintenance_mode/manage_maintenance.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
 function get_alb_rule_arn() {
-  DNS_PREFIX="${ENVIRONMENT}."
-  ALB_ARN=$(aws elbv2 describe-load-balancers --names  ${ENVIRONMENT}-public-front | jq -r .[][]."LoadBalancerArn")
-  LISTENER_ARN=$(aws elbv2 describe-listeners --load-balancer-arn ${LB_ARN} | jq -r '.[][]  | select(.Protocol == "HTTPS") | .ListenerArn')
-  RULE_ARN=$(aws elbv2 describe-rules --listener-arn ${LISTENER_ARN} | jq -r '.[][]  | select(.Priority == "1") | .RuleArn')
+  MM_DNS_PREFIX="${ENVIRONMENT}."
+  MM_ALB_ARN=$(aws elbv2 describe-load-balancers --names  "${ENVIRONMENT}-public-front" | jq -r .[][]."LoadBalancerArn")
+  MM_LISTENER_ARN=$(aws elbv2 describe-listeners --load-balancer-arn ${MM_ALB_ARN} | jq -r '.[][]  | select(.Protocol == "HTTPS") | .ListenerArn')
+  MM_RULE_ARN=$(aws elbv2 describe-rules --listener-arn ${MM_LISTENER_ARN} | jq -r '.[][]  | select(.Priority == "1") | .RuleArn')
   if [ $ENVIRONMENT = "production" ]
   then
-    DNS_PREFIX=""
+    MM_DNS_PREFIX=""
   fi
 }
 
 function enable_maintenance() {
   aws ssm put-parameter --name "${ENVIRONMENT}_enable_maintenance" --type "String" --value "true" --overwrite
   aws elbv2 modify-rule \
-  --rule-arn $RULE_ARN \
-  --conditions Field=host-header,Values="${DNS_PREFIX}claim-power-of-attorney-refund.service.gov.uk"
+  --rule-arn $MM_RULE_ARN \
+  --conditions Field=host-header,Values="${MM_DNS_PREFIX}claim-power-of-attorney-refund.service.gov.uk"
 }
 
 function disable_maintenance() {
   aws ssm put-parameter --name "${ENVIRONMENT}_enable_maintenance" --type "String" --value "false" --overwrite
   aws elbv2 modify-rule \
-  --rule-arn $RULE_ARN \
+  --rule-arn $MM_RULE_ARN \
   --conditions Field=path-pattern,Values='/maintenance'
 }
 
