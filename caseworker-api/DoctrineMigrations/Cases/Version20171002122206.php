eb9222e740e7572f80d86d6930f114df45e6bbba
william Falconer <william.falconer@digital.justice.gov,uk>
2020-12-14 09:04:13 +0000

    remove all .gsi references

--- caseworker-api/DoctrineMigrations/Cases/Version20171002122206.php
+++ caseworker-api/DoctrineMigrations/Cases/Version20171002122206.php
@@ -17,21 +17,21 @@ class Version20171002122206 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('UPDATE "user" SET email = \'caseworker01@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker01@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker02@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker02@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker03@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker03@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker04@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker04@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker05@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker05@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker06@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker06@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker07@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker07@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker08@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker08@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker09@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker09@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker10@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker10@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager01@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager01@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager02@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager02@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager03@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager03@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager04@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager04@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager05@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager05@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker01@publicguardian.gov.uk\' WHERE email = \'caseworker01@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker02@publicguardian.gov.uk\' WHERE email = \'caseworker02@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker03@publicguardian.gov.uk\' WHERE email = \'caseworker03@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker04@publicguardian.gov.uk\' WHERE email = \'caseworker04@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker05@publicguardian.gov.uk\' WHERE email = \'caseworker05@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker06@publicguardian.gov.uk\' WHERE email = \'caseworker06@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker07@publicguardian.gov.uk\' WHERE email = \'caseworker07@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker08@publicguardian.gov.uk\' WHERE email = \'caseworker08@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker09@publicguardian.gov.uk\' WHERE email = \'caseworker09@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker10@publicguardian.gov.uk\' WHERE email = \'caseworker10@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager01@publicguardian.gov.uk\' WHERE email = \'refundmanager01@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager02@publicguardian.gov.uk\' WHERE email = \'refundmanager02@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager03@publicguardian.gov.uk\' WHERE email = \'refundmanager03@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager04@publicguardian.gov.uk\' WHERE email = \'refundmanager04@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager05@publicguardian.gov.uk\' WHERE email = \'refundmanager05@refunds.uat\'');
 
         //  Create admin user
         $adminName = getenv('OPG_REFUNDS_CASEWORKER_ADMIN_NAME');
@@ -48,22 +48,22 @@ class Version20171002122206 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('UPDATE "user" SET email = \'caseworker01@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker01@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker02@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker02@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker03@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker03@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker04@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker04@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker05@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker05@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker06@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker06@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker07@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker07@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker08@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker08@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker09@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker09@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'caseworker10@publicguardian.gsi.gov.uk\' WHERE email = \'caseworker10@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager01@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager01@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager02@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager02@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager03@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager03@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager04@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager04@refunds.uat\'');
-        $this->addSql('UPDATE "user" SET email = \'refundmanager05@publicguardian.gsi.gov.uk\' WHERE email = \'refundmanager05@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker01@publicguardian.gov.uk\' WHERE email = \'caseworker01@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker02@publicguardian.gov.uk\' WHERE email = \'caseworker02@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker03@publicguardian.gov.uk\' WHERE email = \'caseworker03@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker04@publicguardian.gov.uk\' WHERE email = \'caseworker04@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker05@publicguardian.gov.uk\' WHERE email = \'caseworker05@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker06@publicguardian.gov.uk\' WHERE email = \'caseworker06@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker07@publicguardian.gov.uk\' WHERE email = \'caseworker07@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker08@publicguardian.gov.uk\' WHERE email = \'caseworker08@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker09@publicguardian.gov.uk\' WHERE email = \'caseworker09@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'caseworker10@publicguardian.gov.uk\' WHERE email = \'caseworker10@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager01@publicguardian.gov.uk\' WHERE email = \'refundmanager01@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager02@publicguardian.gov.uk\' WHERE email = \'refundmanager02@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager03@publicguardian.gov.uk\' WHERE email = \'refundmanager03@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager04@publicguardian.gov.uk\' WHERE email = \'refundmanager04@refunds.uat\'');
+        $this->addSql('UPDATE "user" SET email = \'refundmanager05@publicguardian.gov.uk\' WHERE email = \'refundmanager05@refunds.uat\'');
 
-        $this->addSql('DELETE FROM "user" WHERE email=\'smtuser01@publicguardian.gsi.gov.uk\'');
+        $this->addSql('DELETE FROM "user" WHERE email=\'smtuser01@publicguardian.gov.uk\'');
     }
 }
