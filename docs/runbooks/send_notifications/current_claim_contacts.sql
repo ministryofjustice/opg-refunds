f2b76e4b8ef961108a52201b9f8d9c5e17d1bbdf
william Falconer <william.falconer@digital.justice.gov,uk>
2021-01-25 15:10:14 +0000

    order by contact type

--- docs/runbooks/send_notifications/current_claim_contacts.sql
+++ docs/runbooks/send_notifications/current_claim_contacts.sql
@@ -1,58 +1,55 @@
 CREATE OR REPLACE TEMPORARY view current_claim_contacts_view AS
-(SELECT
+((SELECT
     json_data -> 'contact' ->> 'email' AS contact,
-    "email" as contact_type,
-    CASE json_data->>applicant
-    WHEN 'attorney' THEN
-    CONCAT
-    (
-        json_data -> 'attorney' -> 'current' -> 'name' ->> 'title' , ' ',
-        json_data -> 'attorney' -> 'current' -> 'name'  ->> 'first' , ' ',
-        json_data -> 'attorney' -> 'current' -> 'name' ->> 'last'
-    )
-    WHEN 'executor' THEN
-    CONCAT
-    (
-        json_data -> 'executor' -> 'name' ->> 'title' , ' ',
-        json_data -> 'executor' -> 'name' ->> 'first' , ' ',
-        json_data -> 'executor' -> 'name' ->> 'last'
-    )
-    WHEN 'donor' then donor_name
-    as claimant_name
-
+    'email' as contact_type,
+    CASE json_data->>'applicant'
+        WHEN 'attorney' THEN
+        CONCAT
+        (
+            json_data -> 'attorney' -> 'current' -> 'name' ->> 'title' , ' ',
+            json_data -> 'attorney' -> 'current' -> 'name'  ->> 'first' , ' ',
+            json_data -> 'attorney' -> 'current' -> 'name' ->> 'last'
+        )
+        WHEN 'executor' THEN
+        CONCAT
+        (
+            json_data -> 'executor' -> 'name' ->> 'title' , ' ',
+            json_data -> 'executor' -> 'name' ->> 'first' , ' ',
+            json_data -> 'executor' -> 'name' ->> 'last'
+        )
+        WHEN 'donor' THEN donor_name
+    END AS claimant_name
 FROM claim
 WHERE status IN ('pending')
-AND json_data -> 'contact' ->> 'receive-notifications' = 'True'
-AND json_data -> 'contact' ->> 'email' IS NOT NULL
+AND json_data -> 'contact' ->> 'receive-notifications' = 'true'
+AND json_data -> 'contact' ->> 'email' IS NOT NULL)
 
 UNION
-SELECT
-    json_data -> 'contact' ->> 'telephone' AS contact,
-    "phone" as contact_type,
-    CASE json_data->>applicant
-    WHEN 'attorney' THEN
-    CONCAT
-    (
-        json_data -> 'attorney' -> 'current' -> 'name' ->> 'title' , ' ',
-        json_data -> 'attorney' -> 'current' -> 'name'  ->> 'first' , ' ',
-        json_data -> 'attorney' -> 'current' -> 'name' ->> 'last'
-    )
-    WHEN 'executor' THEN
-    CONCAT
-    (
-        json_data -> 'executor' -> 'name' ->> 'title' , ' ',
-        json_data -> 'executor' -> 'name' ->> 'first' , ' ',
-        json_data -> 'executor' -> 'name' ->> 'last'
-    )
-    WHEN 'donor' THEN donor_name
-    AS claimant_name
-
+(SELECT
+    json_data -> 'contact' ->> 'phone' as contact,
+    'telephone' as contact_type,
+    CASE json_data->>'applicant'
+        WHEN 'attorney' THEN
+        CONCAT
+        (
+            json_data -> 'attorney' -> 'current' -> 'name' ->> 'title' , ' ',
+            json_data -> 'attorney' -> 'current' -> 'name'  ->> 'first' , ' ',
+            json_data -> 'attorney' -> 'current' -> 'name' ->> 'last'
+        )
+        WHEN 'executor' THEN
+        CONCAT
+        (
+            json_data -> 'executor' -> 'name' ->> 'title' , ' ',
+            json_data -> 'executor' -> 'name' ->> 'first' , ' ',
+            json_data -> 'executor' -> 'name' ->> 'last'
+        )
+        WHEN 'donor' THEN donor_name
+    END AS claimant_name
 FROM claim
 WHERE status IN ('pending')
-AND json_data -> 'contact' ->> 'receive-notifications' = 'True'
+AND json_data -> 'contact' ->> 'receive-notifications' = 'true'
 AND json_data -> 'contact' ->> 'email' IS NULL
-
-
+) order by contact_type
  );
 
 
