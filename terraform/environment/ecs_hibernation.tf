c60fe97fb2a85f80b3a44b7b75ed24027bf29c5a
william Falconer <william.falconer@digital.justice.gov,uk>
2021-02-26 10:00:30 +0000

    make production schedule live

--- terraform/environment/ecs_hibernation.tf
+++ terraform/environment/ecs_hibernation.tf
@@ -1,10 +1,9 @@
 module "daytime" {
   source           = "./modules/ecs_scheduled_scaling"
-  count            = local.account_name == "development" ? 1 : 0
   name             = "daytime"
   ecs_cluster_name = aws_ecs_cluster.lpa_refunds.name
-  scale_down_time  = "cron(00 19 ? * * *)"
-  scale_up_time    = "cron(00 08 ? * * *)"
+  scale_down_time  = "cron(30 22 ? * MON-SAT *)"
+  scale_up_time    = "cron(30 05 ? * MON-SAT *)"
   service_config = {
     tostring(aws_ecs_service.caseworker_api.name) = {
       scale_down_to = 0
