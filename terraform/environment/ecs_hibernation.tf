242ce13dc76a6a7524e8d46c1bc0bb99eac9e811
William Falconer <williamfalconeruk@users.noreply.github.com>
2021-03-01 10:19:22 +0000

    Lpal 313 create schedule scaling for all environments. (#166)
    * Add module code
    
    * see if this clashes with the other autoscale rule?
    
    * fix min as not acceptable at this point
    
    * add vars for setting desired counts
    
    * make production schedule live
    
    * split prod and non prod schedules
    
    * refactor to account based schedule. bit cleaner.
    
    * typo
    
    * typo again
    
    * now with RDS control, using external scaling module.
    
    * terraform fmt
    
    * Schedule adjusted to mon - fri on non prod envs
    
    Co-authored-by: william Falconer <william.falconer@digital.justice.gov,uk>

--- terraform/environment/ecs_hibernation.tf
+++ terraform/environment/ecs_hibernation.tf
@@ -1,10 +1,9 @@
 module "daytime" {
   source           = "./modules/ecs_scheduled_scaling"
-  count            = local.account_name == "development" ? 1 : 0
   name             = "daytime"
   ecs_cluster_name = aws_ecs_cluster.lpa_refunds.name
-  scale_down_time  = "cron(00 19 ? * * *)"
-  scale_up_time    = "cron(00 08 ? * * *)"
+  scale_down_time  = local.account.scale_down_schedule
+  scale_up_time    = local.account.scale_up_schedule
   service_config = {
     tostring(aws_ecs_service.caseworker_api.name) = {
       scale_down_to = 0
@@ -24,3 +23,4 @@ module "daytime" {
     }
   }
 }
+
