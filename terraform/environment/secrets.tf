e65468c9f826ad75bbbb0c3918f3cc7babfdeaa6
william Falconer <william.falconer@digital.justice.gov.uk>
2021-04-29 14:57:22 +0100

    remove secret

--- terraform/environment/secrets.tf
+++ terraform/environment/secrets.tf
@@ -122,6 +122,3 @@ data "aws_secretsmanager_secret" "opg_refunds_sscl_analysis" {
   name = "${local.account_name}/opg_refunds_sscl_analysis"
 }
 
-data "aws_secretsmanager_secret" "opg_refunds_public_key" {
-  name = "${local.account_name}/opg_refunds_public_key"
-}
