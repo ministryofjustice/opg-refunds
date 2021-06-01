f70068fc72345418448d08812a90f6cab013ae08
william Falconer <william.falconer@digital.justice.gov.uk>
2021-06-01 11:52:58 +0100

    remove slack links from messages

--- .circleci/set_slack_env_vars.sh
+++ .circleci/set_slack_env_vars.sh
@@ -1,7 +1,2 @@
 #! /bin/bash
-
-echo 'export FRONT_DOMAIN="$(jq -r .public_front_fqdn /tmp/environment_pipeline_tasks_config.json)"'
-echo 'export CASEWORKER_DOMAIN="$(jq -r .caseworker_front_fqdn /tmp/environment_pipeline_tasks_config.json)"'
-echo 'export CLAIM_SERVICE_GOV_UK="$(jq -r .claim_service_gov_uk_fqdn /tmp/environment_pipeline_tasks_config.json)"'
-echo 'export CASEWORKER_REFUNDS_OPG_DIGITAL="$(jq -r .caseworker_refunds_opg_digital_fqdn /tmp/environment_pipeline_tasks_config.json)"'
 echo 'export COMMIT_MESSAGE="$(git log -1 --pretty=%B)"'
