b3132410367dd34d70f8b5ce02b24df854c95782
william Falconer <william.falconer@digital.justice.gov.uk>
2021-06-18 13:31:39 +0100

    add kms keys for SSE on S3 and data export - fixes

--- terraform/account/archive.tf
+++ terraform/account/archive.tf
@@ -1,8 +1,23 @@
-# allow public read on the bucket and objects,
-# but access will be limited by policy to moj_sites
+#KMS key for Server Side Encryption (SSE) of S3 buckets
+resource "aws_kms_key" "kms_refunds_s3_archive_key"{
+  description             = "This key is used to encrypt S3 bucket objects"
+  deletion_window_in_days = 10
+}
+
+#archive bucket - enable SSE by default
 resource "aws_s3_bucket" "refunds_archive" {
   bucket = "refunds-${terraform.workspace}-caseworker-archive"
   acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.kms_refunds_s3_archive_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
   tags   = merge(local.default_tags)
 
 }
@@ -55,7 +70,7 @@ resource "aws_s3_bucket_policy" "refunds_archive_policy" {
 # see https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_ExportSnapshot.html#USER_ExportSnapshot.SetupIAMRole
 resource "aws_iam_role" "refunds_caseworker_export_role" {
   name               = "refunds-caseworker-export-role"
-  assume_role_policy = data.aws_iam_policy_document.refunds_caseworker_export_assume_role_policy
+  assume_role_policy = data.aws_iam_policy_document.refunds_caseworker_export_assume_role_policy.json
   tags               = merge(local.default_tags)
 }
 
@@ -75,7 +90,7 @@ data "aws_iam_policy_document" "refunds_caseworker_export_assume_role_policy" {
 resource "aws_iam_policy" "refunds_caseworker_export_policy" {
   name        = "refunds-caseworker-export-policy"
   description = "this policy allows export of RDS snapshots to an S3 bucket"
-  policy      = data.aws_iam_policy_document.refunds_caseworker_export_policy
+  policy      = data.aws_iam_policy_document.refunds_caseworker_export_policy.json
 }
 
 #policy document setting out access for export role
@@ -99,6 +114,46 @@ data "aws_iam_policy_document" "refunds_caseworker_export_policy" {
 
 # create a policy attachment for the export role.
 resource "aws_iam_role_policy_attachment" "refunds_caseworker_export_role_policy_attachment" {
-  role       = aws_iam_role.refunds_caseworker_export_role
-  policy_arn = aws_iam_policy.refunds_caseworker_export_policy
+  role       = aws_iam_role.refunds_caseworker_export_role.name
+  policy_arn = aws_iam_policy.refunds_caseworker_export_policy.arn
+}
+
+# Add aws CMK for encrypting the data.
+resource "aws_kms_key" "refunds_caseworker_db_archive_key" {
+  description             = "Refunds caseworker archive key"
+  deletion_window_in_days = 10
+  policy                  = data.aws_iam_policy_document.kms_refunds_caseworker_db_archive_key.json
+}
+
+#allow only breakglass role to run this - we do not need op access.
+data "aws_iam_policy_document" "kms_refunds_caseworker_db_archive_key" {
+  statement {
+    sid    = "KMS admin"
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${local.account.account_id}:role/breakglass"
+      ]
+    }
+    resources = ["*"]
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt",
+      "kms:GenerateDataKey*",
+      "kms:Create*",
+      "kms:Describe*",
+      "kms:Enable*",
+      "kms:List*",
+      "kms:Put*",
+      "kms:Update*",
+      "kms:Revoke*",
+      "kms:Disable*",
+      "kms:Get*",
+      "kms:Delete*",
+      "kms:ScheduleKeyDeletion",
+      "kms:CancelKeyDeletion"
+    ]
+  }
 }
