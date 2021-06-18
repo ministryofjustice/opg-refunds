f30b5599e6fec22586fa46873b4753435f7517b0
william Falconer <william.falconer@digital.justice.gov.uk>
2021-06-18 12:30:00 +0100

    add export roles and policies

--- terraform/account/archive.tf
+++ terraform/account/archive.tf
@@ -51,3 +51,54 @@ resource "aws_s3_bucket_policy" "refunds_archive_policy" {
 
 
 
+# set up RDS to S3 export roles
+# see https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/USER_ExportSnapshot.html#USER_ExportSnapshot.SetupIAMRole
+resource "aws_iam_role" "refunds_caseworker_export_role" {
+  name               = "refunds-caseworker-export-role"
+  assume_role_policy = data.aws_iam_policy_document.refunds_caseworker_export_assume_role_policy
+  tags               = merge(local.default_tags)
+}
+
+# Assume role policy for export role
+data "aws_iam_policy_document" "refunds_caseworker_export_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["export.rds.amazonaws.com"]
+    }
+  }
+}
+
+# set up IAM policy for export
+resource "aws_iam_policy" "refunds_caseworker_export_policy" {
+  name        = "refunds-caseworker-export-policy"
+  description = "this policy allows export of RDS snapshots to an S3 bucket"
+  policy      = data.aws_iam_policy_document.refunds_caseworker_export_policy
+}
+
+#policy document setting out access for export role
+data "aws_iam_policy_document" "refunds_caseworker_export_policy" {
+  statement {
+    sid    = "ExportPolicy"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject*",
+      "s3:ListBucket",
+      "s3:GetObject*",
+      "s3:DeleteObject*",
+      "s3:GetBucketLocation"
+    ]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.refunds_archive.arn}",
+      "arn:aws:s3:::${aws_s3_bucket.refunds_archive.arn}/*"
+    ]
+  }
+}
+
+# create a policy attachment for the export role.
+resource "aws_iam_role_policy_attachment" "refunds_caseworker_export_role_policy_attachment" {
+  role       = aws_iam_role.refunds_caseworker_export_role
+  policy_arn = aws_iam_policy.refunds_caseworker_export_policy
+}
